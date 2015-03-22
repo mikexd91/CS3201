dd425e3a5557e542bfc7f4bbcbe03157f1e9e8b5
Jonathan Chiam <jczjchiam@gmail.com>
2015-03-22 15:00:51 +0800

    Header and method stubs for StmtTable

---
+++ source/StmtTable.cpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <boost/unordered_map.hpp>
+
+using namespace std;
+using namespace boost;
+
+#include "StmtTable.h"
+#include "AST.h"
+#include "TNode.h"
+// to include CFG
+// to include GNode
+
+// adds statement to table
+// 
+void StmtTable::addStmt(int stmtNum, string type) {
+
+
+}
+
+// get type of statement
+string StmtTable::getStmtType(int stmtNum) {
+
+
+}
+
+// get AST node rep of statement
+TNode * getASTReference(int stmtNum) {
+
+
+}
+
+/*
+// get CFG node rep of statement
+GNode * getCFGReference(int stmtNum) {
+
+
+}*/
+
+// sets the AST ref node of statement
+void setASTReference(int stmtNum, TNode node) {
+
+
+}
+
+/*
+// sets the CFG ref node of statement
+void setCFGReference(int stmtNum, GNode node) {
+
+
+}*/
---
+++ source/StmtTable.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <boost/unordered_map.hpp>
+
+using namespace std;
+using namespace boost;
+
+#include "AST.h"
+#include "TNode.h"
+// to include CFG
+// to include GNode
+
+class StmtTable {
+
+public:
+	StmtTable();	// constructor: instantiates an empty statement table
+
+	void addStmt(int stmtNum, string type); // adds statement to table
+
+	string getStmtType(int stmtNum);		// get type of statement
+	TNode * getASTReference(int stmtNum);	// get AST node rep of statement
+	//GNode * getCFGReference(int stmtNum);	// get CFG node rep of statement
+
+	void setASTReference(int stmtNum, TNode node);	// sets the AST ref node of statement
+	//void setCFGReference(int stmtNum, GNode node);	// sets the CFG ref node of statement
+
+private:
+	//unordered_map<int, vector<unordered_map<>>> _stmtTable;
+
+};
