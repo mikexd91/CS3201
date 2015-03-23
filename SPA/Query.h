df27922a1fb6f53048302bf56f7c3639224b8361
Andrew Low <andrewjrlow@gmail.com>
2015-03-24 02:44:31 +0800

    query header

--- SPA/Query.h
+++ SPA/Query.h
@@ -1,14 +1,24 @@
 #pragma once
 #include <string>
-//Query object to be passed between parser and evaluator
+#include <vector>
+#include "Clause.h"
 using std::string;
-
+using std::vector;
 class Query
 {
 private:
 	string selectSynonym;
+	int numClauses;
+	vector<Clause> clauseList;
 public:
 	Query(void);
 	~Query(void);
-};
+	
+	void setSelectSynonym(string);
+	void setClauseList(vector<Clause>);
+	void addClause(Clause);
 
+	string getSelectSynonym();
+	vector<Clause> getClauseList();
+	Clause getClause(int);
+};
