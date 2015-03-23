79330e31bd02a25f7030d9749084cee47beb1346
heyjiawei <jaewei.j.w@gmail.com>
2015-03-23 16:49:42 +0800

    Shifted files to source

--- source/Query.h
+++ source/Query.h
@@ -1,25 +1,14 @@
 #pragma once
 #include <string>
-#include <vector>
-#include "Clause.h"
+//Query object to be passed between parser and evaluator
 using std::string;
-using std::vector;
+
 class Query
 {
 private:
 	string selectSynonym;
-	int numClauses;
-	vector<Clause> clauseList;
 public:
 	Query(void);
 	~Query(void);
-	
-	void setSelectSynonym(string);
-	void setClauseList(vector<Clause>);
-	void addClause(Clause);
-
-	string getSelectSynonym();
-	vector<Clause> getClauseList();
-	Clause getClause(int);
 };
 
