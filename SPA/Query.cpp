89c7a4e8630dee2081f8bbd332fb4b9db321a8ee
Andrew Low <andrewjrlow@gmail.com>
2015-03-24 02:56:19 +0800

    Query

--- SPA/Query.cpp
+++ SPA/Query.cpp
@@ -1,5 +1,7 @@
 #include "Query.h"
-
+#include "Clause.h"
+#include <string>
+#include <vector>
 
 Query::Query(void){
 
@@ -8,4 +10,34 @@ Query::Query(void){
 
 Query::~Query(void)
 {
+}
+
+void Query::setSelectSynonym(string synonym){
+	this->selectSynonym = synonym;
+}
+
+void Query::setClauseList(vector<Clause> list){
+	this->clauseList = list;
+	int sizeOf = list.size();
+	this->numClauses = sizeOf;
+}
+
+void Query::addClause(Clause item){
+	this->clauseList.push_back(item);
+}
+
+string Query::getSelectSynonym(){
+	return this->selectSynonym;
+}
+
+vector<Clause> Query::getClauseList(){
+	return this->clauseList;
+}
+
+Clause Query::getClause(int index){
+	return this->clauseList.at(index);
+}
+
+int Query::getNumClauses(){
+	return this->numClauses;
 }
