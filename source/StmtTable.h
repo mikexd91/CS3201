69b6bf3030548cd0bee8c38b712f3523764fe797
Jonathan Chiam <jczjchiam@gmail.com>
2015-03-23 16:00:13 +0800

    Modifications to StmtTable

--- source/StmtTable.h
+++ source/StmtTable.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <set>
 #include <boost/unordered_map.hpp>
 
 using namespace boost;
@@ -10,6 +11,37 @@ using namespace boost;
 // to include GNode
 
 class StmtTable {
+	/*
+
+	Conceptual StmtTable:
+	------------------------------------------------------------
+	|_[key]______|______________________|______________________|
+	| stmt no.	 | TNode representation | GNode representation |
+	|_[key]______|______________________|______________________|
+	------------------------------------------------------------
+
+	Actual Implementation:
+	2 separate tables -
+
+	TNode Table - contains the TNode (AST) representation of the stmts
+	-----------------------------------------
+	|_[key]__________|______________________|
+	| stmt no. (int) | TNodes (set<TNode>)  | 
+	-----------------------------------------
+
+	GNode Table - contains the GNode (CFG) representation of the stmts
+	-----------------------------------------
+	|_[key]__________|______________________|
+	| stmt no. (int) | GNodes (set<GNode>)  | 
+	-----------------------------------------
+
+	*/
+
+	typedef set<TNode> TNodeRefTableRow;
+	//typedef set<GNode>	GNodeRefTableRow;
+
+	typedef boost::unordered_map<int, TNodeRefTableRow>		TNodeRefTable;
+	//typedef boost::unordered_map<int, GNodeRefTableRow>	GNodeRefTableRow;
 
 public:
 	StmtTable();	// constructor: instantiates an empty statement table
