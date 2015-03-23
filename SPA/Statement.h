9f4455750b1ca6ececdc1f6e90b7448a120550e2
Jonathan Chiam <jczjchiam@gmail.com>
2015-03-23 16:55:25 +0800

    Updated StmtTable and created Statement headers + settings update

---
+++ SPA/Statement.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <set>
+
+using namespace std;
+
+/*
+// --------------------------- Statement --------------------------- 
+// Statement object created by PDR to use to add to statement table
+// 1. PDR create statement objecy
+// 2. Set all necessary components
+// 3. Add to StmtTable
+// -----------------------------------------------------------------
+*/
+class Statement {
+
+
+
+
+
+
+
+}
