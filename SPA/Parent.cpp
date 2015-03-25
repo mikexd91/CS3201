89a70758e5d57dcdf0c78b1edbd271bd4c5dcecc
heyjiawei <jaewei.j.w@gmail.com>
2015-03-25 20:38:04 +0800

    Updated error files. Expect error from Follows.cpp and Result.cpp as they are not fixed yet

--- SPA/Parent.cpp
+++ SPA/Parent.cpp
@@ -1,76 +1,11 @@
-#pragma once
 #include "Parent.h"
-<<<<<<< HEAD
-#include "StmtTable.h"
-=======
->>>>>>> 6ef66e5d505603d41310ab5f77408e4d9b36deb8
 
-using namespace std;
-// constructor: instantiates an empty parent table
-Parent::Parent() {
-	
-}
 
-/*
-// checks if s1 is parent of s2
-bool isParent(int stmtNum1, int stmtNum2) {
-	ParentTable parentTable = ParentTable(); // need to clarify this
-	vector<int> results = parentTable.getStmtChildren(stmtNum1);
-	vector<int>::iterator it;
-	it = find(results.begin(), results.end(), stmtNum2);
-
-	if (results.empty() || (it != results.end())) {
-		return false;
-	} else {
-		return true;
-	}
-}
-
-// get list of parents of stmtNum
-vector<int> getParent(int stmtNum) {
-	ParentTable parentTable = ParentTable();
-	vector<int> results = parentTable.getStmtParents(stmtNum);
-	return results;
-}
-
-// get list of children of stmtNum
-vector<int> getChild(int stmtNum) {
-	ParentTable parentTable = ParentTable();
-	vector<int> results = parentTable.getStmtChildren(stmtNum);
-	return results;
+Parent::Parent(void)
+{
 }
 
 
-// gets lists of all possible parent-child combinations
-vector<vector<int>> getAllParent(syn syn1, syn syn2) {
-
-
-} 
-
-// checks if s1 is parent* of s2
-bool isParentStar(int stmtNum1, int stmtNum2) {
-	// get parent of stmtNum2
-	// check if parent == stmtNum1
-	// if stmtNum 1 parent returns none, return false
-	// if yes, return true
-	// else, get parent of stmtNum1 (do recursion)
+Parent::~Parent(void)
+{
 }
-
-// get list of parents* of stmtNum
-vector<int> getParentStar(int stmtNum) {
-
-
-}
-
-// get list of children* of stmtNum
-vector<int> getChildStarBy(int stmtNum) {
-
-
-}
-
-
-// gets lists of all possible parent-child* combinations
-vector<vector<int>> getAllParentStar(syn syn1, syn syn2) {
-
-
-} */
--- SPA/Parent.h
+++ SPA/Parent.h
@@ -1,39 +1,8 @@
 #pragma once
-
-<<<<<<< HEAD
-#include <vector>     
-#include "StmtTable.h"
-=======
-#include <vector>
-#include <boost/unordered_map.hpp>
->>>>>>> 6ef66e5d505603d41310ab5f77408e4d9b36deb8
-
-using namespace std;
-
-class Parent {
-
+class Parent
+{
 public:
-	Parent();									// constructor: instantiates an empty parent table
-
-	bool isParent(int stmtNum1, int stmtNum2);	// checks if s1 is parent of s2
-
-	vector<int> getParent(int stmtNum);			// get list of parents of stmtNum
-	vector<int> getChild(int stmtNum);			// get list of children of stmtNum
-
-	//vector<vector<int>> getAllParent(syn syn1, syn syn2);			// gets lists of all possible parent-child combinations
-
-	bool isParentStar(int stmtNum1, int stmtNum2);	// checks if s1 is parent* of s2
-
-	vector<int> getParentStar(int stmtNum);		// get list of parents* of stmtNum
-	vector<int> getChildStarBy(int stmtNum);	// get list of children* of stmtNum
-
-	//vector<vector<int>> getAllParentStar(syn syn1, syn syn2);		// gets lists of all possible parent-child* combinations
-
-private:
-<<<<<<< HEAD
-	StmtTable stmtTable;
-}
-=======
-	
+	Parent(void);
+	~Parent(void);
 };
->>>>>>> 6ef66e5d505603d41310ab5f77408e4d9b36deb8
+
