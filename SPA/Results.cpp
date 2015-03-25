45d6064fbf0602b4d1a1bf52fefda217f296d1a3
heyjiawei <jaewei.j.w@gmail.com>
2015-03-25 11:21:27 +0800

    Updated results class
    left with toString method

---
+++ SPA/Results.cpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "Results.h"
+
+Results::Results(void)
+{
+}
+
+
+Results::~Results(void)
+{
+}
+
+void Results::addStmt(int stmt) {
+	stmtResultSet.push_back(stmt);
+}
+	
+void Results::addVar(string var) {
+	varResultSet.push_back(var);
+}
+
+void Results::setResultType(string type) {
+	this->resultType = type;
+}
+
+vector<int> Results::getStmtResults() {
+	return this->stmtResultSet;
+}
+	
+vector<string> Results::getVarResults() {
+	return this->varResultSet;
+}
+	
+string Results::toString() {
+	if (this->resultType.compare("stmt")) {
+
+	} else if (this->resultType.compare("var")) {
+
+	} else {
+
+	}
+}
