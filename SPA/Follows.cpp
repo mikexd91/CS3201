12b19cfed9522c2597015a7ea0736ee1c45813ab
heyjiawei <jaewei.j.w@gmail.com>
2015-03-28 19:20:55 +0800

    Fixed known bugs in Follows

--- SPA/Follows.cpp
+++ SPA/Follows.cpp
@@ -14,8 +14,8 @@ Follows::~Follows(void)
 }
 
 Statement* getStmtObj(int stmtnum) {
-	StmtTable* table = StmtTable.getInstance();
-	const set<Statement*> set = table->getAssgStmts(); // error?
+	StmtTable* table = StmtTable::getInstance();
+	const set<Statement*> set = table->getAssgStmts();
 	for (setIter=set.begin(); setIter!=set.end(); setIter++) {
 		Statement* stmt = *setIter;
 		int stmtnum2 = stmt->getStmtNum();
@@ -23,11 +23,12 @@ Statement* getStmtObj(int stmtnum) {
 			return stmt;
 		}
 	}
+	return 0;
 }
 
 bool Follows::isFollows(int stmtNum1, int stmtNum2) {
 	int stmt = getFollows(stmtNum1);
-	if (stmt == stmtNum2) {
+	if (stmt != 0 && stmt == stmtNum2) {
 		return true;
 	} else {
 		return false;
@@ -37,44 +38,72 @@ bool Follows::isFollows(int stmtNum1, int stmtNum2) {
 // gets immediate statement after stmtNum
 int Follows::getFollows(int stmtNum) {
 	Statement* stmtObj = getStmtObj(stmtNum);
-	set<int> stmtsSet = stmtObj->getFollows();
-	intIter = stmtsSet.begin();
-	int stmt = *intIter;
-	return stmt;
+	if (stmtObj != 0) {
+		set<int> stmtsSet = stmtObj->getFollows();
+		if (!stmtsSet.empty()) {
+			intIter = stmtsSet.begin();
+			int stmt = *intIter;
+			return stmt;
+		}
+	} 
+	return 0;
 }
 
 // gets immediate statement before stmtNum
 int Follows::getFollowedBy(int stmtNum) {
 	Statement* stmtObj = getStmtObj(stmtNum);
-	set<int> stmtsSet = stmtObj->getFollowedBy();
-	intIter = stmtsSet.end();
-	int stmt = *intIter;
-	return stmt;
+	if (stmtObj != 0) {
+		set<int> stmtsSet = stmtObj->getFollowedBy();
+		if (!stmtsSet.empty()) {
+			intIter = stmtsSet.end();
+			int stmt = *intIter;
+			return stmt;
+		}
+	}
+	return 0;
 }
 
+/*
 set<set<int>> Follows::getAllFollows(int stmtNum1, int stmtNum2) {
 
 }
-
+*/
 
 bool Follows::isFollowsStar(int stmtNum1, int stmtNum2) {
 	set<int> stmtList = getFollowsStar(stmtNum1);
+	if (!stmtList.empty()) {
+		for (intIter=stmtList.begin(); intIter!=stmtList.end(); intIter++) {
+			int stmtNum = *intIter;
+			if (stmtNum == stmtNum2) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
 // gets all those before b in Follows(a, b)
 set<int> Follows::getFollowedStarBy(int stmtNum) {
 	Statement* stmtObj = getStmtObj(stmtNum);
-	set<int> stmtsSet = stmtObj->getFollowedBy();
-	return stmtsSet;
+	if (stmtObj != 0) {
+		set<int> stmtsSet = stmtObj->getFollowedBy();
+		return stmtsSet;
+	}
+	return set<int>();
 }
 
 // gets all those after a in Follows(a, b)
 set<int> Follows::getFollowsStar(int stmtNum) {
 	Statement* stmtObj = getStmtObj(stmtNum);
-	set<int> stmtsSet = stmtObj->getFollows();
-	return stmtsSet;
+	if (stmtObj != 0) {
+		set<int> stmtsSet = stmtObj->getFollows();
+		return stmtsSet;
+	}
+	return set<int>();
 }
 
+/*
 set<set<int>> Follows::getAllFollowsStar(int stmtNum1, int stmtNum2) {
 
 }
+*/
