4ae70d7adda9e4c95bb70791ec0c0c3f074dcf50
Jonathan Chiam <jczjchiam@gmail.com>
2015-03-31 22:30:46 +0800

    Added test case for getStmtObj function in StmtTable

--- UnitTesting/source/StmtTableTest.h
+++ UnitTesting/source/StmtTableTest.h
@@ -5,6 +5,7 @@
 class StmtTableTest : public CPPUNIT_NS::TestFixture { 
 	CPPUNIT_TEST_SUITE( StmtTableTest );
 	CPPUNIT_TEST( testInstance );
+	CPPUNIT_TEST( testGetStmtByNum );
 	CPPUNIT_TEST( testGetAssgStmts );
 	CPPUNIT_TEST( testGetCallStmts );
 	CPPUNIT_TEST( testGetWhileStmts );
@@ -17,6 +18,7 @@ public:
 	void tearDown();
 
 	void testInstance();			// method to test singleton instance getter
+	void testGetStmtByNum();		// method to test retrieval of statement by statement number
 	void testGetAssgStmts();		// method to test retrieval of assignment statements
 	void testGetCallStmts();		// method to test retrieval of call statements
 	void testGetWhileStmts();		// method to test retrieval of while statements
