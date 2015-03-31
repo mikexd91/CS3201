692236a9527c8e1d35b2e4674eb8e2fd2b493928
Nicholas Lum <nicholaslum444@hotmail.com>
2015-03-31 15:54:21 +0800

    Added the prognode test

--- UnitTesting/ProgNodeTest.cpp
+++ UnitTesting/ProgNodeTest.cpp
@@ -20,14 +20,28 @@ CPPUNIT_TEST_SUITE_REGISTRATION( ProgNodeTest );
 
 void ProgNodeTest::testProperties() { 
 
-	
+	// constructor
+	ProgNode pnode("Alice");
+	CPPUNIT_ASSERT_EQUAL(NodeType::PROGRAM_, pnode.getNodeType());
+	string expname = "Alice";
+	CPPUNIT_ASSERT_EQUAL(expname, pnode.getName());
 
 	return;
 }
 
 void ProgNodeTest::testLinks() {
 
-	
+	ProgNode pnode("Alice");
+	ProcNode wnode("BobProc");
+
+	// linkprocnode
+	pnode.linkProcNode(&wnode);
+
+	// getprocnodes
+	CPPUNIT_ASSERT_EQUAL((TNode*)(&wnode), pnode.getProcNodes().at(0));
+	ProcNode proc = *((ProcNode*)(pnode.getProcNodes().at(0)));
+	string expname = "BobProc";
+	CPPUNIT_ASSERT_EQUAL(expname, proc.getName());
 
 	return;
 }
