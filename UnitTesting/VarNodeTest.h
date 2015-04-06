08e113215091d99fb6231959a16c2406070d05d1
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-07 02:29:38 +0800

    Rename main unitest file and made varnode test
    1. rename pkbunittest to allunittest. everything goes there now
    2. create varnode test because im stupid and forgot to make it last time

---
+++ UnitTesting/VarNodeTest.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// Note 1
+#include <cppunit/extensions/HelperMacros.h>
+
+class VarNodeTest : public CPPUNIT_NS::TestFixture {
+	CPPUNIT_TEST_SUITE( VarNodeTest );
+	CPPUNIT_TEST( testProperties );
+	CPPUNIT_TEST( testLinks );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+
+	void testProperties();
+
+	void testLinks();
+};
+    
