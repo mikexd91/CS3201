0f8704ddf2e5b72e1d08f32e524bdcc97e6a9ea2
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-06 18:45:27 +0800

    Added comments to the testwrapper on what to do

--- AutoTester/source/TestWrapper.cpp
+++ AutoTester/source/TestWrapper.cpp
@@ -21,13 +21,23 @@ TestWrapper::TestWrapper() {
 void TestWrapper::parse(std::string filename) {
 	// call your parser to do the parsing
 	// ...rest of your code...
+
+	// TODO
+	// read the file as given by filename
+	// call parser.parse on the string
+
 }
 
 // method to evaluating a query
 void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
 	// call your evaluator to evaluate the query here
 	// ...code to evaluate query...
-
 	// store the answers to the query in the results list (it is initially empty)
 	// each result must be a string.
+
+	// TODO
+	// eval the query
+	// get results
+	// iterate through the results and stuff them into the results list
+
 }
