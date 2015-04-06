b1a4726e84347e232463396de84652e4cd370b83
Pan Peining <peining_pan@hotmail.com>
2015-04-06 16:26:55 +0800

    Yay it built!

--- IntegrationTesting/source/TestOne.h
+++ IntegrationTesting/source/TestOne.h
@@ -4,7 +4,7 @@
 
 class TestOne : public CPPUNIT_NS::TestFixture { 
 	CPPUNIT_TEST_SUITE( TestOne );
-	//CPPUNIT_TEST( testAddProc );
+	CPPUNIT_TEST( testAddProc );
 	CPPUNIT_TEST( testAssign );
 	CPPUNIT_TEST( testFalseAddProc );
 	CPPUNIT_TEST_SUITE_END();
