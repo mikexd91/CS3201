187c75054da129cae39582241e5b3b68ad0208ff
Pan Peining <peining_pan@hotmail.com>
2015-04-06 17:29:06 +0800

    Added another test for integration testing

--- IntegrationTesting/source/TestOne.cpp
+++ IntegrationTesting/source/TestOne.cpp
@@ -23,16 +23,25 @@ CPPUNIT_TEST_SUITE_REGISTRATION( TestOne );
 
 // method to test adding of proc to table
 void TestOne::testAddProc() {
-	parser.parse("procedure test {x = 2; y=3;}");
+	parser.parse("procedure test {x = 2; y=x+z;}");
 	ProcTable* procTable = ProcTable::getInstance();
 	CPPUNIT_ASSERT(procTable->contains("test"));
 	StmtTable* stmtTable = StmtTable::getInstance();
 	
-	Statement* stmt = stmtTable->getStmtObj(1);
+	Statement* stmt1 = stmtTable->getStmtObj(1);
+	string initUses1[] = { "x" };
+	set<string> str1(initUses1, initUses1 + 1);
+	CPPUNIT_ASSERT(stmt1->getModifies() == str1);
 	
-	string init[] = { "x" };
-	set<string> str(init, init + 1);
-	CPPUNIT_ASSERT(stmt->getModifies() == str);
+	Statement* stmt2 = stmtTable->getStmtObj(2);
+	string initModifies2[] = { "y" };
+	set<string> strModifies2(initModifies2, initModifies2 + 1);
+	CPPUNIT_ASSERT(stmt2->getModifies() == strModifies2);
+	//string initUses2[] = { "x", "z" };
+	set<string> strUses2;
+	strUses2.insert("x");
+	strUses2.insert("z");
+	//CPPUNIT_ASSERT(stmt2->getUses() == strUses2);
 }
 
 void TestOne::testAssign() {
