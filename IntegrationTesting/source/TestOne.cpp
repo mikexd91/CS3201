cc04d0c9b9123645f8bd0e4f41003c69569e8a7d
Pan Peining <peining_pan@hotmail.com>
2015-04-06 20:04:14 +0800

    Added in more tests, not working currently

--- IntegrationTesting/source/TestOne.cpp
+++ IntegrationTesting/source/TestOne.cpp
@@ -2,7 +2,6 @@
 #include "TestOne.h"
 #include "../../SPA/Parser.h"
 #include "../../SPA/PDR.h"
-#include "../../SPA/ProcTable.h"
 
 // TEST VARIABLES
 
@@ -23,10 +22,11 @@ CPPUNIT_TEST_SUITE_REGISTRATION( TestOne );
 
 // method to test adding of proc to table
 void TestOne::testAddProc() {
-	parser.parse("procedure test {x = 2; y=x+z; z=x+y;}");
+	parser.parse("procedure test {x = 2; y=x+z; z=x+y+z;}");
 	ProcTable* procTable = ProcTable::getInstance();
 	CPPUNIT_ASSERT(procTable->contains("test"));
 	StmtTable* stmtTable = StmtTable::getInstance();
+	VarTable* varTable = VarTable::getInstance();
 	
 	Statement* stmt1 = stmtTable->getStmtObj(1);
 	string initUses1[] = { "x" };
@@ -45,10 +45,37 @@ void TestOne::testAddProc() {
 	string initModifies3[] = { "z" };
 	set<string> strModifies3(initModifies3, initModifies3 + 1);
 	CPPUNIT_ASSERT(stmt3->getModifies() == strModifies3);
+	
 	string initUses3[] = { "x", "y" };
 	set<string> strUses3(initUses3, initUses3 + 2);
 	CPPUNIT_ASSERT(stmt3->getUses() == strUses3);
+
+	Variable* varX = varTable->getVariable("x");
+	int initXUsedBy[] = {2, 3};
+	int initXModifiedBy[] = {1};
+	set<int> setXUsedBy(initXUsedBy, initXUsedBy + 2);
+	set<int> setXModifiedBy(initXModifiedBy, initXModifiedBy + 1);
+	CPPUNIT_ASSERT(varX->getUsedByStmts() == setXUsedBy);
+	CPPUNIT_ASSERT(varX->getModifiedByStmts() == setXModifiedBy);
+
+	Variable* varY = varTable->getVariable("y");
+	int initYUsedBy[] = {3};
+	int initYModifiedBy[] = {2};
+	set<int> setYUsedBy(initYUsedBy, initYUsedBy + 1);
+	set<int> setYModifiedBy(initYModifiedBy, initYModifiedBy + 1);
+	CPPUNIT_ASSERT(varY->getUsedByStmts() == setYUsedBy);
+	CPPUNIT_ASSERT(varY->getModifiedByStmts() == setYModifiedBy);
+
+	Variable* varZ = varTable->getVariable("z");
+	int initZUsedBy[] = {2, 3};
+	int initZModifiedBy[] = {3};
+	set<int> setZUsedBy(initZUsedBy, initZUsedBy + 2);
+	set<int> setZModifiedBy(initZModifiedBy, initZModifiedBy + 1);
+	CPPUNIT_ASSERT(varZ->getUsedByStmts() == setZUsedBy);
+	CPPUNIT_ASSERT(varZ->getModifiedByStmts() == setZModifiedBy);
 }
+
+
 
 void TestOne::testAssign() {
 	
