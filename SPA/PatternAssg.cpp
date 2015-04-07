12298d7bd335e45bad4b5d17c5f6075349465776
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-08 01:56:46 +0800

    Implemented the patternassg class
    1. the patternassg class returns whether a match is correct
    2. the patternassg class only does this for assignments
    3. this implementation should likely be ported over to the new clauses
    classes
    4. test case for this test is also complete
    5. not in prototype scope but this matching can also work for times and
    brackets, as long as rpn is used

---
+++ SPA/PatternAssg.cpp
@@ -0,0 +1,138 @@
+#include "PatternAssg.h"
+#include <iostream>
+#include <stack>
+#include "OpNode.h"
+//#include "boost/lockfree/stack.hpp"
+
+//using namespace boost;
+//using namespace boost::lockfree;
+using namespace std;
+
+PatternAssg::PatternAssg(int stmtNum) {
+	this->_stmtNum = stmtNum;
+}
+
+PatternAssg::~PatternAssg() {
+
+}
+
+bool PatternAssg::matchNothing() {
+	return true;
+}
+
+bool PatternAssg::matchVar(string var) {
+	// look for assg stmts with var on the left hand side
+	// basically look for modifies var.
+	StmtTable* stable = StmtTable::getInstance();
+	set<Statement*> assgStmts = stable->getAssgStmts();
+
+	set<Statement*>::iterator iter;
+	for (iter = assgStmts.begin(); iter != assgStmts.end(); iter++) {
+		Statement* assg = *iter;
+		if (assg->getStmtNum() == _stmtNum) {
+			set<string> uses = assg->getModifies();
+			if (uses.find(var) != uses.end()) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	return false;
+}
+
+bool PatternAssg::match(string var, string expr) {
+	// find the stmt that match this var
+	// then search its subtree for the expr
+	StmtTable* stable = StmtTable::getInstance();
+	set<Statement*> assgStmts = stable->getAssgStmts();
+
+	set<Statement*>::iterator iter;
+	for (iter = assgStmts.begin(); iter != assgStmts.end(); iter++) {
+		Statement* assg = *iter;
+		if (assg->getStmtNum() == _stmtNum) {
+			set<string> uses = assg->getModifies();
+			if (uses.find(var) != uses.end()) {
+				return matchExpr((AssgNode*)(assg->getTNodeRef()), expr);
+				//return false;
+			} else {
+				return false;
+			}
+		}
+	}
+
+	return false;
+}
+
+
+bool PatternAssg::matchExpr(AssgNode* assg, string expr) {
+	// match expr based on the assgnode given
+	if (assg == NULL) {
+		return false;
+	}
+	int i = 0;
+
+	// stuff the rpn into the stack
+	string rpn = expr.substr(2, expr.length() - 4);
+	//std::stack<string> tempstack = stack<string>();
+	//std::stack<string> rpnstack = stack<string>();
+	std::vector<string> rpnarr = vector<string>();
+	for (int i = 0; i < rpn.length(); i++) {
+		string token = rpn.substr(i, 1);
+		if (token != " ") {
+			//tempstack.push(token);
+			rpnarr.push_back(token);
+		}
+	}
+
+	// do dfs
+	set<TNode*> visited;
+	stack<TNode*> nodestack = stack<TNode*>();
+	TNode* exprnode = assg->getExprNode();
+	cout << exprnode->getName() << endl;
+	nodestack.push(exprnode);
+	TNode* nextnode = nodestack.top();
+	while (nextnode != NULL && i < 9999) {
+		i++;
+		if (nextnode->getNodeType() == OPERATOR_) {
+			OpNode* op = (OpNode*) nextnode;
+			nodestack.push(op->getRightNode());
+			nodestack.push(op->getLeftNode());
+			nextnode = nodestack.top();
+			cout << nextnode->getName() << endl;
+		} else if (nextnode->getNodeType() == CONSTANT_) {
+			nextnode = NULL;
+		} else if (nextnode->getNodeType() == VARIABLE_) {
+			nextnode = NULL;
+		} else {
+			return false;
+		}
+	}
+
+	// compare rpn with dfs nodes
+	// once start to match rpn must match all the way
+	// then only considered subtree
+	for (int compared = 0; compared < rpnarr.size() && i < 9999; compared++) {
+		if (nodestack.empty()) {
+			return false;
+		}
+		TNode* node = nodestack.top();
+		string token = rpnarr.at(compared);
+		//cout << "conparing" << endl;
+		//cout << node->getName() << endl;
+		//cout << token << endl;
+		if (node->getName() == token) {
+			nodestack.pop();
+			// if we break a streak of matches,
+			// then it's not subtree
+		} else {
+			// start from the front of the rpn again
+			// -1 cos it will ++ on the iteration
+			compared = -1;
+			nodestack.pop();
+		}
+	}
+
+	return true;
+}
