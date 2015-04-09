76d0ef5c595a6d26f7ea0256f589bd520213df51
Pan Peining <peining_pan@hotmail.com>
2015-04-09 13:35:07 +0800

    Shifted RPN method into utils

--- SPA/Utils.cpp
+++ SPA/Utils.cpp
@@ -4,14 +4,12 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <queue>
+#include <stack>
+
+#include "InvalidExpressionException.h"
 
 using namespace std;
-
-Utils::Utils(void){
-}
-
-Utils::~Utils(void){
-}
 
 string Utils::sanitise(string str) {
 	for (size_t i = 0; i < strlen(UtilsConstants::SANITISED_CHARS); i++) {
@@ -22,20 +20,90 @@ string Utils::sanitise(string str) {
 }
 
 //Split code into a vector of tokens
-vector<string> Utils::explode(const string &str) {
+vector<string> Utils::explode(const string &str, const string delim_string, const char delimiters[7] ) {
+	char delimitersToUse[7]; 
+	for (int i = 0; i < 7; i++) {
+		delimitersToUse[i] = delimiters[i];
+	}
+
 	vector<string> elems;
 	int pos;
 	int prev = 0;
-	while ((pos = str.find_first_of(UtilsConstants::DELIM_STRING, prev)) != string::npos) {
+	while ((pos = str.find_first_of(delim_string, prev)) != string::npos) {
 		//if there are words in between (not consecutive delimiters)
 		if (pos > prev) {
 			elems.push_back(str.substr(prev, pos - prev));
 		}
 		//if it is a delimiter that should be included (aka all delimiters but spaces)
-		if (find(begin(UtilsConstants::DELIMITERS), end(UtilsConstants::DELIMITERS), str[pos]) != end(UtilsConstants::DELIMITERS)) {
+		if (find(begin(delimitersToUse), end(delimitersToUse), str[pos]) != end(delimitersToUse)) {
 			elems.push_back(string(1, str[pos]));
 		}
 		prev = pos + 1;
 	}
 	return elems;
+}
+
+queue<string> Utils::getRPN(queue<string> originalExpression) {
+	stack<string> operationStack;
+	queue<string> expressionQueue;
+	//using Shunting-yard algorithm
+	string word;
+	int count = 0;
+	word = getWordAndPop(originalExpression);
+	parseFactor(word, expressionQueue);
+	while (!originalExpression.empty()) {
+		word = getWordAndPop(originalExpression);
+		parseSymbol(word, expressionQueue, operationStack);
+		word = getWordAndPop(originalExpression);
+		parseFactor(word, expressionQueue);
+	}
+	while (!operationStack.empty()) {
+		expressionQueue.push(operationStack.top());
+		operationStack.pop();
+	}
+	return expressionQueue;
+}
+
+void Utils::parseSymbol(string word, queue<string> &expressionQueue, stack<string> &operationStack) {
+	//if top of stack is *, all other operation (+-*) are lower or equal, so just add top to output queue
+	//if top of stack is + or -, only add top to output queue if word is + or -
+	if (isValidSymbol(word)) {
+		while (!operationStack.empty() && !(operationStack.top() != "*" && word == "*")) {
+			expressionQueue.push(operationStack.top());
+			operationStack.pop();
+		}
+		operationStack.push(word);
+	} else {
+		throw InvalidExpressionException("Invalid Expression!");
+	}
+}
+
+bool Utils::isValidName(string name) {
+	if (!isalpha(name[0])) {
+		return false;
+	} else {
+		for (int i = 1; i < name.size(); i++) {
+			if(!isalnum(name[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+bool Utils::isValidConstant(string number) {
+	for (int i = 0; i < number.size(); i++) {
+		if (!isdigit(number[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool Utils::isValidFactor(string factor) {
+	return isValidConstant(factor) || isValidName(factor);
+}
+
+bool Utils::isValidSymbol(string symbol) {
+	return symbol == "+" || symbol == "-" || symbol == "*";
 }
--- SPA/Utils.h
+++ SPA/Utils.h
@@ -2,22 +2,30 @@
 #include <string>
 #include <vector>
 #include <regex>
+#include <queue>
+#include <stack>
 
-using std::string;
-using std::vector;
+using namespace std;
+
 class Utils
 {
 public:
-	Utils(void);
-	~Utils(void);
 	
-	string sanitise(string str);
-	vector<string> explode(const string &str);
+	static string sanitise(string str);
+	static vector<string> explode(const string&, const string, const char[7]);
+	static queue<string> getRPN(queue<string>);
+	static string getWordAndPop(queue<string>&);
+	static void parseFactor(string, queue<string>&);
+	static void parseSymbol(string, queue<string>&, stack<string>&);
+	static bool isValidFactor(string);
+	static bool isValidSymbol(string);
+	static bool isValidName(string);
+	static bool isValidConstant(string);
 	
 };
 
 namespace UtilsConstants {
-	const string DELIM_STRING = " ;={}";
-	const char DELIMITERS[] = { ';', '=', '{', '}' };
+	//const string DELIM_STRING = " ;={}";
+	//const char DELIMITERS[] = { ';', '=', '{', '}' };
 	const char SANITISED_CHARS[] = { '\t', '\n' };
 };
