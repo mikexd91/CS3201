8759e2d58e4588eb07ae4601d1053fb6cfce9de9
Jonathan Chiam <jczjchiam@gmail.com>
2015-04-10 00:32:57 +0800

    Premature implementation of uses

--- SPA/UsesClause.cpp
+++ SPA/UsesClause.cpp
@@ -1,5 +1,12 @@
 #include "UsesClause.h"
 #include "Utils.h"
+#include "StmtTable.h"
+#include "Results.h"
+
+#include <set>
+
+StmtTable* stmtTable = StmtTable::getInstance();
+
 UsesClause::UsesClause(void):Clause(USES_){
 }
 
@@ -14,6 +21,53 @@ bool UsesClause::isValid(void){
 	return (firstArg && secondArg);
 }
 
+// ONLY EVALUATES PROTOTYPE CASES (dealing with assg stmts and vars)
 Results UsesClause::evaluate(void) {
+	// first arg properties
+	string firstArgType = this->getFirstArgType();
+	bool firstArgIsFixed = this->getFirstArgFixed();
+
+	// second arg properties
+	string secondArgType = this->getSecondArgType();
+	bool secondArgIsFixed = this->getSecondArgFixed();
+	
+	// ASSIGN CASES
+	if(firstArgType==stringconst::ARG_ASSIGN && secondArgType==stringconst::ARG_VARIABLE) {
+		// get set of assignment stmts and iterator
+		set<Statement*> assgStmtSet = stmtTable->getAssgStmts();
+		set<Statement*>::iterator assgIter;
+
+		// Assign Case: Uses(a,v) - assign wild, var wild
+		if(firstArgIsFixed==false && secondArgIsFixed==false) {
+
+		
+		// Assign Case: Uses(a,'x') - assign wild, var fixed
+		} else if(firstArgIsFixed==false && secondArgIsFixed==true) {
+
+
+		// Assign Case: Uses(1,v) - assign fixed, var wild
+		} else if(firstArgIsFixed==true && secondArgIsFixed==false) {
+
+
+		// Assign Case: Uses(1,'x') - assign fixed, var fixed
+		} else {
+			int stmtNum = stoi(this->getFirstArg(), nullptr);
+			for(assgIter=assgStmtSet.begin(); assgIter!=assgStmtSet.end(); assgIter++) {
+				// current stmt
+				Statement* currentStmt = *assgIter;
+				if(currentStmt->getStmtNum() == stmtNum) {
+					// get set of variables current stmt uses
+					Statement::UsesSet currentUses = currentStmt->getUses();
+					
+					// checks if current stmt uses variable
+					if(currentUses.find(this->getSecondArg()) != currentUses.end()) {
+						Results res = Results();
+						res.setClauseTrue();
+					}
+				}
+			}
+		}
+	}
+
 	return Results();
 }
--- SPA/UsesClause.h
+++ SPA/UsesClause.h
@@ -1,10 +1,19 @@
 #pragma once
+
 #include "Clause.h"
 class UsesClause : public Clause{
 public:
 	UsesClause(void);
 	~UsesClause(void);
+
 	bool isValid(void);
 	Results evaluate(void);
+
+private:
+	// ONLY EVALUATES PROTOTYPE CASES
+	Results evaluateStmtWildVarWild();
+	Results evaluateStmtWildVarFixed();
+	Results evaluateStmtFixedVarWild();
+	Results evaluateStmtFixedVarFixed();
 };
 
