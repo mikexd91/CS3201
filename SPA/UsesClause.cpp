4d01b8ddbc5c9c92ee3259aa7e92d9109dfb9620
Andrew Low <andrewjrlow@gmail.com>
2015-04-09 12:19:50 +0800

    grammar for uses

--- SPA/UsesClause.cpp
+++ SPA/UsesClause.cpp
@@ -10,7 +10,7 @@ bool UsesClause::isValid(void){
 	string firstType = this->getFirstArgType();
 	string secondType = this->getSecondArgType();
 	bool firstArg = (firstType == stringconst::ARG_STATEMENT) || (firstType == stringconst::ARG_ASSIGN) || (firstType == stringconst::ARG_WHILE);
-	bool secondArg = (secondType == stringconst::ARG_STATEMENT) || (secondType == stringconst::ARG_ASSIGN) || (secondType == stringconst::ARG_WHILE);
+	bool secondArg = (secondType == stringconst::ARG_VARIABLE);
 	return (firstArg && secondArg);
 }
 
