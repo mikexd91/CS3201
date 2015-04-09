4607f58c7f4cf882566dcee034ef6eb564d8e845
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-09 18:41:28 +0800

    Started on the modclause with test env set up

---
+++ UnitTesting/ModifiesClauseTest.cpp
@@ -0,0 +1,202 @@
+#include <cppunit/config/SourcePrefix.h>
+#include "ModifiesClauseTest.h"
+#include "../SPA/PatternAssgClause.h"
+#include "../SPA/AST.h"
+#include "../SPA/AssgNode.h"
+#include "../SPA/ConstNode.h"
+#include "../SPA/OpNode.h"
+#include "../SPA/StmtTable.h"
+#include "../SPA/VarTable.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void ModifiesClauseTest::setUp() {
+	/* testing this source
+	procedure zumba {
+		i = 1+2;	//1
+		j = 2+3+4;	//2
+		k = 3;		//3
+		w = i;		//4
+		x = w+k;	//5
+	}
+	*/
+
+	// to set up the ast manually
+	AST* ast = AST::getInstance();
+
+	ProcNode* proc = new ProcNode("zumba");
+	StmtLstNode* procsl = new StmtLstNode();
+	proc->linkStmtLstNode(procsl);
+
+	AssgNode* assg1 = new AssgNode(1);
+	VarNode* i1 = new VarNode("i");
+	assg1->linkVarNode(i1);
+	OpNode* plus1 = new OpNode("+");
+	ConstNode* one1 = new ConstNode("1");
+	ConstNode* two1 = new ConstNode("2");
+	plus1->linkLeftNode(one1);
+	plus1->linkRightNode(two1);
+	assg1->linkExprNode(plus1);
+	procsl->linkStmtNode(assg1);
+
+	AssgNode* assg2 = new AssgNode(2);
+	VarNode* j2 = new VarNode("j");
+	OpNode* plus2_1 = new OpNode("+");
+	ConstNode* four2 = new ConstNode("4");
+	OpNode* plus2_2 = new OpNode("+");
+	ConstNode* three2 = new ConstNode("3");
+	ConstNode* two2 = new ConstNode("2");
+	plus2_2->linkRightNode(three2);
+	plus2_2->linkLeftNode(two2);
+	plus2_1->linkRightNode(four2);
+	plus2_1->linkLeftNode(plus2_2);
+	assg2->linkVarNode(j2);
+	assg2->linkExprNode(plus2_1);
+	procsl->linkStmtNode(assg2);
+
+	AssgNode* assg3 = new AssgNode(3);
+	VarNode* k3 = new VarNode("k");
+	ConstNode* three3 = new ConstNode("3");
+	assg3->linkVarNode(k3);
+	assg3->linkExprNode(three3);
+	procsl->linkStmtNode(assg3);
+
+	AssgNode* assg4 = new AssgNode(4);
+	VarNode* w4 = new VarNode("w");
+	assg4->linkVarNode(w4);
+	VarNode* i4 = new VarNode("i");
+	assg4->linkExprNode(i4);
+	procsl->linkStmtNode(assg4);
+
+	AssgNode* assg5 = new AssgNode(5);
+	VarNode* x5 = new VarNode("x");
+	assg5->linkVarNode(x5);
+	OpNode* plus5 = new OpNode("+");
+	VarNode* w5 = new VarNode("w");
+	VarNode* k5 = new VarNode("k");
+	plus5->linkLeftNode(w5);
+	plus5->linkRightNode(k5);
+	assg5->linkExprNode(plus5);
+	procsl->linkStmtNode(assg5);
+
+	ast->addProcNode(proc);
+
+	// to set up the stmttable manually
+	StmtTable* stable = StmtTable::getInstance();
+
+	Statement* stmt1 = new Statement();
+	stmt1->setStmtNum(1);
+	stmt1->setType(ASSIGN_STMT_);
+	stmt1->setFollowsAfter(2);
+	string ivar = "i";
+	set<string> mods1 = set<string>();
+	mods1.emplace(ivar);
+	stmt1->setModifies(mods1);
+	stmt1->setTNodeRef(assg1);
+	stable->addStmt(stmt1);
+
+	Statement* stmt2 = new Statement();
+	stmt2->setStmtNum(2);
+	stmt2->setType(ASSIGN_STMT_);
+	stmt2->setFollowsBefore(1);
+	stmt2->setFollowsAfter(3);
+	string jvar = "j";
+	set<string> mods2 = set<string>();
+	mods2.emplace(jvar);
+	stmt2->setModifies(mods2);
+	stmt2->setTNodeRef(assg2);
+	stable->addStmt(stmt2);
+	
+	Statement* stmt3 = new Statement();
+	stmt3->setStmtNum(3);
+	stmt3->setType(ASSIGN_STMT_);
+	stmt3->setFollowsBefore(2);
+	string kvar = "k";
+	set<string> mods3 = set<string>();
+	mods3.emplace(kvar);
+	stmt3->setModifies(mods3);
+	stmt3->setTNodeRef(assg3);
+	stable->addStmt(stmt3);
+
+	Statement* stmt4 = new Statement();
+	stmt4->setStmtNum(4);
+	stmt4->setType(ASSIGN_STMT_);
+	stmt4->setFollowsBefore(3);
+	stmt4->setFollowsAfter(5);
+	string wvar = "w";
+	set<string> mods4 = set<string>();
+	mods4.emplace(wvar);
+	stmt4->setModifies(mods4);
+	set<string> uses4 = set<string>();
+	uses4.emplace(ivar);
+	stmt4->setUses(uses4);
+	stmt4->setTNodeRef(assg4);
+	stable->addStmt(stmt4);
+
+	Statement* stmt5 = new Statement();
+	stmt5->setStmtNum(5);
+	stmt5->setType(ASSIGN_STMT_);
+	stmt5->setFollowsBefore(4);
+	string xvar = "x";
+	set<string> mods5 = set<string>();
+	mods5.emplace(xvar);
+	stmt5->setModifies(mods5);
+	set<string> uses5 = set<string>();
+	uses5.emplace(wvar);
+	uses5.emplace(kvar);
+	stmt5->setUses(uses5);
+	stmt5->setTNodeRef(assg5);
+	stable->addStmt(stmt5);
+
+	// to set up the vartable manually
+	VarTable* vtable = VarTable::getInstance();
+
+	Variable* vi = new Variable("i");
+	vi->addModifyingStmt(1);
+	vi->addUsingStmt(4);
+	vi->addTNode(i1);
+	vi->addTNode(i4);
+	vtable->addVariable(vi);
+
+	Variable* vj = new Variable("j");
+	vj->addModifyingStmt(2);
+	vj->addTNode(j2);
+	vtable->addVariable(vj);
+
+	Variable* vk = new Variable("k");
+	vk->addModifyingStmt(2);
+	vk->addUsingStmt(5);
+	vk->addTNode(k3);
+	vtable->addVariable(vk);
+
+	Variable* vw = new Variable("w");
+	vw->addModifyingStmt(4);
+	vw->addUsingStmt(5);
+	vw->addTNode(w4);
+	vtable->addVariable(vw);
+
+	Variable* vx = new Variable("x");
+	vx->addModifyingStmt(5);
+	vx->addTNode(x5);
+	vtable->addVariable(vx);
+
+
+}
+
+void ModifiesClauseTest::tearDown() {
+	// to clear the pkb
+	AST::reset();
+	StmtTable::getInstance()->clearTable();
+	VarTable::
+}
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( ModifiesClauseTest );
+
+
+void ModifiesClauseTest::testModifiesNumNum() {
+
+}
---
+++ UnitTesting/ModifiesClauseTest.h
@@ -0,0 +1,30 @@
+#pragma once
+
+// Note 1
+#include <cppunit/extensions/HelperMacros.h>
+
+class ModifiesClauseTest : public CPPUNIT_NS::TestFixture {
+	CPPUNIT_TEST_SUITE( ModifiesClauseTest );
+	CPPUNIT_TEST( testModifiesNumNum );
+	//CPPUNIT_TEST( evaulateVarWildExpr );
+	//CPPUNIT_TEST( evaluateVarFixedExprWild );
+	//CPPUNIT_TEST( evaluateVarFixedExpr );
+	//CPPUNIT_TEST( evaluateVarExprWild );
+	//CPPUNIT_TEST( evaluateVarExpr );
+	CPPUNIT_TEST_SUITE_END();
+
+public:
+	void setUp();
+	void tearDown();
+	
+	void testModifiesNumNum();
+
+	/*void evaluateVarWildExprWild();
+	void evaulateVarWildExpr();
+	void evaluateVarFixedExprWild();
+	void evaluateVarFixedExpr();
+	void evaluateVarExprWild();
+	void evaluateVarExpr();*/
+
+};
+    
