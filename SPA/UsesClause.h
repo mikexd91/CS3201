07c0dcbf60c45685c8e538aca4cd139890b7abad
Jonathan Chiam <jczjchiam@gmail.com>
2015-04-10 12:53:21 +0800

    Naive implementation of uses complete

--- SPA/UsesClause.h
+++ SPA/UsesClause.h
@@ -11,9 +11,8 @@ public:
 
 private:
 	// ONLY EVALUATES PROTOTYPE CASES
-	Results evaluateStmtWildVarWild();
-	Results evaluateStmtWildVarFixed();
-	Results evaluateStmtFixedVarWild();
-	Results evaluateStmtFixedVarFixed();
+	Results evaluateStmtWildVarWild();			// Case: Uses(s,v) - stmt wild, var wild
+	Results evaluateStmtWildVarFixed();			// Case: Uses(s,'x') - stmt wild, var fixed
+	Results evaluateStmtFixedVarWild();			// Case: Uses(1,v) - stmt fixed, var wild
+	Results evaluateStmtFixedVarFixed();		// Case: Uses(1,'x') - stmt fixed, var fixed
 };
-
