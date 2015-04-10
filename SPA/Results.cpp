27ab03802c91ce161479bcfaa29cba50824f9f1e
heyjiawei <jaewei.j.w@gmail.com>
2015-04-10 15:53:48 +0800

    Updated Results class. Shouldn't affect any clauses or any tests

--- SPA/Results.cpp
+++ SPA/Results.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Results.h"
+#include <algorithm>
 
 Results::Results(void)
 {
@@ -35,6 +36,14 @@ void Results::setSecondClauseSyn(string s2) {
 	this->secondClauseSyn = s2;
 }
 
+void Results::setSingleResult(vector<string> v) {
+	this->singleResults = v;
+}
+
+void Results::setPairResult(vector<pair<string, string>> v) {
+	this->pairResults = v;
+}
+
 bool Results::isClausePassed() {
 	return this->clausePassed;
 }
@@ -59,8 +68,69 @@ int Results::getNumOfSyn() {
 	return this->numOfSyn;
 }
 
-void Results::getIntersect(Results res) {
+void Results::getSingleIntercept(Results &res1, Results &res2) {
+	string syn1 = res1.getFirstClauseSyn();
+	string syn2 = res2.getFirstClauseSyn();
+
+	if (syn1 == syn2) {
+		vector<string> res1List = res1.getSinglesResults();
+		vector<string> res2List = res2.getSinglesResults();
+		vector<string> temp = *new vector<string>();
+
+		for (int i = 0; i < res1List.size(); i++) {
+			for (int j = 0; j < res2List.size(); j++) {
+				if (res1List.at(i) == res2List.at(j)) {
+					temp.push_back(res1List.at(i));
+				}
+			}
+		}
 	
+		res1.setSingleResult(temp);
+		res2.setSingleResult(temp);
+	}
+}
+
+void Results::getSinglePairIntercept(Results &res1, Results &res2) {
+
+}
+
+void Results::getPairIntercept(Results &res1, Results &res2) {
+	string res1Syn1 = res1.getFirstClauseSyn();
+	string res1Syn2 = res1.getSecondClauseSyn();
+
+	string res2Syn1 = res2.getFirstClauseSyn();
+	string res2Syn2 = res2.getSecondClauseSyn();
+
+	if (res1Syn1 == res2Syn1) {			// assign or statement
+
+	} else if (res1Syn2 == res2Syn1) {	// assign
+
+	} else if (res1Syn2 == res2Syn2) {	// variable
+
+	} else {
+		// error
+	}
+}
+
+void Results::getIntersect(Results &res) {
+	int nSynRes1 = this->getNumOfSyn();
+	int nSynRes2 = res.getNumOfSyn();
+
+	if (nSynRes1 == 1 && nSynRes2 == 1) {
+		getSingleIntercept(*this, res);
+
+	} else if (nSynRes1 == 1 && nSynRes2 == 2) {
+		getSinglePairIntercept(*this, res);
+
+	} else if (nSynRes1 == 2 && nSynRes2 == 1) {
+		res.getIntersect(*this);
+
+	} else if (nSynRes1 == 2 && nSynRes1 == 2) {
+		getPairIntercept(*this, res);
+
+	} else {
+		// error
+	}
 }
 
 set<string> Results::getSelectSynResult(string syn) {
