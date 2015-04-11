48546edffaac7b1f3977de67fec30819c2dcddc6
heyjiawei <jaewei.j.w@gmail.com>
2015-04-11 21:32:26 +0800

    Boost linker error. Need to exclude tests to allow testing

--- UnitTesting/QueryEvaluatorTest.cpp
+++ UnitTesting/QueryEvaluatorTest.cpp
@@ -1,20 +1,26 @@
 #include <cppunit/config/SourcePrefix.h>
 #include "QueryEvaluatorTest.h"
 #include "../SPA/Results.h"
+#include "../SPA/QueryEvaluator.h"
 
 using namespace std;
 
 // Registers the fixture into the 'registry'
 CPPUNIT_TEST_SUITE_REGISTRATION( QueryEvaluatorTest );
 
+// Test getSameClause of QueryEvaluator
 void QueryEvaluatorTest::testEvaluator() {
-	Results* resObj1 = new Results();
-	Results* resObj2 = new Results();
+	QueryEvaluator e = *new QueryEvaluator();
+	Results r1 = *new Results();
+	Results r2 = *new Results();
 
-	resObj1->setClausePassed(true);
-	resObj2->setClausePassed(true);
+	r1.setNumOfSyn(0);
+	r2.setNumOfSyn(0);
 
-	resObj1->
+	int num = e.getSameClause(r1, r2);
+	
+	cout << "JIAWEI";
+	cout << num;
 }
 
 void QueryEvaluatorTest::testEvaluator2() {
