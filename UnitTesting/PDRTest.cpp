7438d7af4a52d69e87045bc68b71ccd1cb5ef8d9
Leon Chin <leonchin499@gmail.com>
2015-04-12 21:31:47 +0800

    PDR unit testers

---
+++ UnitTesting/PDRTest.cpp
@@ -0,0 +1,65 @@
+#include <cppunit/config/SourcePrefix.h>
+#include "PDRTest.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+	PDR* pdr;
+	VarTable* varTable;
+	ProcTable* procTable;
+	StmtTable* stmtTable;
+
+void PDRTest::setUp() {
+	pdr = PDR::getInstance();
+	varTable = VarTable::getInstance();
+	procTable = ProcTable::getInstance();
+	stmtTable = StmtTable::getInstance();
+}
+
+void PDRTest::tearDown() {
+	PDR::resetInstanceFlag();
+	VarTable::reset();
+	procTable->clearTable();
+	stmtTable->clearTable();
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION( PDRTest );
+
+void PDRTest::testProcessProc() {
+	ParsedData procedure = ParsedData(ParsedData::PROCEDURE, 0);
+	procedure.setProcName("proc");
+
+	pdr->processParsedData(procedure);
+
+	CPPUNIT_ASSERT(pdr->getCurrNestingLevel() == 1);
+	CPPUNIT_ASSERT(pdr->getCurrStmtNumber() == 0);
+
+	stack<TNode*> nodeStack = pdr->getNodeStack();
+
+	CPPUNIT_ASSERT(nodeStack.size() == 2);
+	CPPUNIT_ASSERT(nodeStack.top()->getNodeType() == NodeType::STMTLST_);
+	
+	TNode* procStmtLst = nodeStack.top();
+	nodeStack.pop();
+	CPPUNIT_ASSERT(nodeStack.top()->getNodeType() == NodeType::PROCEDURE_);
+	CPPUNIT_ASSERT(nodeStack.top()->getName() == "proc");
+	CPPUNIT_ASSERT(procStmtLst->getParent() == nodeStack.top());
+}
+
+void PDRTest::testProcessAssign() {
+	ParsedData procedure = ParsedData(ParsedData::PROCEDURE, 0);
+	procedure.setProcName("proc");
+	pdr->processParsedData(procedure);
+
+	ParsedData assign1 = ParsedData(ParsedData::ASSIGNMENT, 1);
+	assign1.setAssignVar("x");
+	queue<string> assign1Exp;
+	assign1Exp.push("2");
+	assign1.setAssignExpression(assign1Exp);
+	pdr->processParsedData(assign1);
+
+	CPPUNIT_ASSERT(pdr->getCurrNestingLevel() == 1);
+	CPPUNIT_ASSERT(pdr->getCurrStmtNumber() == 1);
+}
