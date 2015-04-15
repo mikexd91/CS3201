a21bb967647d26c38609cdebebf2f6ac98bc6880
Jonathan Chiam <jczjchiam@gmail.com>
2015-04-16 01:26:01 +0800

    Added get methods for ConstTable

--- SPA/ConstTable.h
+++ SPA/ConstTable.h
@@ -25,6 +25,8 @@ public:
 	ConstantTable::iterator getIterator();		// gets iterator to const table
 	ConstantTable::iterator getEnd();			// gets iterator to end of const table
 
+	Constant* getConst(const string &name);		// get const by name
+	Constant* getConst(int value);				// get const by value
 	vector<Constant*> getAllConst();			// gets all const objects
 	vector<string*> getAllConstNames();			// gets all const names
 	vector<int> getAllConstValues();			// gets all const values
