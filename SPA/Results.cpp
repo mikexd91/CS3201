0a6ef1e690ca75335f58d8f7d6297d2629f676a3
heyjiawei <jaewei.j.w@gmail.com>
2015-08-21 16:46:03 +0800

    Results class done

--- SPA/Results.cpp
+++ SPA/Results.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-bool clausePassed = false;
-
 Results::Results(void)
 {
 }
@@ -25,320 +23,317 @@ void Results::resetClausePass() {
 	this->clausePassed = false;
 }
 
-vector<bool> isExist(vector<string> synList) {
-	return vector<bool>();
-}
+/*
+This is a helper function used by selectMultiSyn.
+It helps retrieve all values in row that have the same synonym
+as that in synList.
+*/
+unordered_map<string, string> Results::getSynResults(unordered_map<string, string> row, vector<string> synList) {
+	unordered_map<string, string> results;
+	string value;
 
-vector<unordered_map<string, string>> selectMultiSyn(vector<string> synList) {
-	return vector<unordered_map<string, string>>();
-}
-
-vector<string> selectSyn(string syn) {
-	return vector<string>();
-}
-
-vector<unordered_map<string, string>> selectSynWhere(string syn, string val, vector<unordered_map<string, string>>) {
-	return vector<unordered_map<string, string>>();
-}
-
-bool insertMultiResult(unordered_map<string, string> results) {
-	return false;
-}
-
-bool insertResult(string syn, string value) {
-	return false;
-}
-
-bool delMultiResult(unordered_map<string, string> results) {
-	return false;
-}
-
-bool delResult(string syn, string value) {
-	return false;
+	for (vector<string>::iterator j = synList.begin(); j != synList.end(); ++j) {
+		value = row[*j];
+		
+		if (value.length() > 0) {
+			results[*j] = value;
+		} 
+	}
+	
+	return results;
 }
 
 /*
-void Results::setClausePassed(bool passed) {
-	this->clausePassed = passed;
-}
-
-void Results::setNumOfSyn(int n) {
-	this->numOfSyn = n;
-}
-
-void Results::addSingleResult(string s) {
-	this->singleResults.push_back(s);
-}
-
-void Results::addPairResult(string s1, string s2) {
-	pair<string, string> pair = make_pair(s1, s2);
-	this->pairResults.push_back(pair);
-}
-
-void Results::setFirstClauseSyn(string s1) {
-	this->firstClauseSyn = s1;
-}
-
-void Results::setSecondClauseSyn(string s2) {
-	this->secondClauseSyn = s2;
-}
-
-void Results::setSingleResult(vector<string>& v) {
-	this->singleResults = v;
-}
-
-void Results::setPairResult(vector<pair<string, string>>& v) {
-	this->pairResults = v;
-}
-
-bool Results::isClausePassed() {
-	return this->clausePassed;
-}
-
-string Results::getFirstClauseSyn() {
-	return this->firstClauseSyn;
-}
-
-string Results::getSecondClauseSyn() {
-	return this->secondClauseSyn;
-}
-
-vector<string> Results::getSinglesResults() {
-	return this->singleResults;
-}
-
-vector<pair<string, string>> Results::getPairResults() {
-	return this->pairResults;
-}
-
-int Results::getNumOfSyn() {
-	return this->numOfSyn;
-}
-
-void Results::getSingleIntercept(Results &res1, Results &res2) {
-	string syn1 = res1.getFirstClauseSyn();
-	string syn2 = res2.getFirstClauseSyn();
-
-	if (syn1 == syn2) {
-		vector<string> res1List = res1.getSinglesResults();
-		vector<string> res2List = res2.getSinglesResults();
-		vector<string> temp = *new vector<string>();
-
-		for (size_t  i = 0; i < res1List.size(); i++) {
-			for (size_t  j = 0; j < res2List.size(); j++) {
-				if (res1List.at(i) == res2List.at(j)) {
-					temp.push_back(res1List.at(i));
-				}
-			}
-		}
-		res1.setSingleResult(temp);
-		res2.setSingleResult(temp);
-	}
-}
-
-void Results::getSinglePairIntercept(Results &res1, Results &res2) {
-	string res1Syn1 = res1.getFirstClauseSyn();
-	string res2Syn1 = res2.getFirstClauseSyn();
-	string res2Syn2 = res2.getSecondClauseSyn();
-	vector<string> res1List = res1.getSinglesResults();
-	vector<pair<string, string>> res2List = res2.getPairResults();
-	set<string> temp1 = *new set<string>();
-	set<pair<string, string>> temp2 = *new set<pair<string, string>>();
-
-	bool modifiedFlag = false;
-
-	if (res1Syn1 == res2Syn1) {
-		modifiedFlag = true;
-		for (size_t  i = 0; i < res1List.size(); i++) {
-			for (size_t  j = 0; j < res2List.size(); j++) {
-
-				if (res1List.at(i) == res2List.at(j).first) {
-					temp1.emplace(res1List.at(i));
-					temp2.emplace(res2List.at(j));
-				}
-			}
-		}
-
-	} else if (res1Syn1 == res2Syn2) {
-		modifiedFlag = true;
-		for (size_t  i = 0; i < res1List.size(); i++) {
-			for (size_t  j = 0; j < res2List.size(); j++) {
-				
-				if (res1List.at(i) == res2List.at(j).second) {
-					temp1.emplace(res1List.at(i));
-					temp2.emplace(res2List.at(j));
-				}
-			}
-		}
-	} 
-
-	if (modifiedFlag) {
-		res1.setSingleResult(vector<string>());
-		res2.setPairResult(vector<pair<string, string>>());
-		
-		for (set<string>::iterator iter = temp1.begin(); iter != temp1.end(); iter++) {
-			res1.addSingleResult(*iter);
-		}
-
-		for (set<pair<string, string>>::iterator iter = temp2.begin(); iter != temp2.end(); iter++) {
-			pair<string, string> p = *iter;
-			res2.addPairResult(p.first, p.second);
-		}
-	}
-}
-
-void Results::getPairIntercept(Results &res1, Results &res2) {
-	string res1Syn1 = res1.getFirstClauseSyn();
-	string res1Syn2 = res1.getSecondClauseSyn();
-	string res2Syn1 = res2.getFirstClauseSyn();
-	string res2Syn2 = res2.getSecondClauseSyn();
-
-	vector<pair<string, string>> res1List = res1.getPairResults();
-	vector<pair<string, string>> res2List = res2.getPairResults();
-	set<pair<string, string>> temp1 = *new set<pair<string, string>>();
-	set<pair<string, string>> temp2 = *new set<pair<string, string>>();
-
-	bool resultsModifiedFlag = false;
-
-	if (res1Syn1 == res2Syn1 && res1Syn2 == res2Syn2) {
-		resultsModifiedFlag = true;
-		for (size_t  i = 0; i < res1List.size(); i++) {
-			for (size_t  j = 0; j < res2List.size(); j++) {
-
-				if ((res1List.at(i).first == res2List.at(j).first) &&
-					(res1List.at(i).second == res2List.at(j).second)){
-					temp1.emplace(res1List.at(i));
-					temp2.emplace(res2List.at(j));
-				}
-			}
-		}
-
-	} else if (res1Syn1 == res2Syn1) {
-		resultsModifiedFlag = true;
-		for (size_t  i = 0; i < res1List.size(); i++) {
-			for (size_t  j = 0; j < res2List.size(); j++) {
-
-				if (res1List.at(i).first == res2List.at(j).first) {
-					temp1.emplace(res1List.at(i));
-					temp2.emplace(res2List.at(j));
-				}
-			}
-		}
-
-	} else if (res1Syn2 == res2Syn1) {
-		resultsModifiedFlag = true;
-		for (size_t  i = 0; i < res1List.size(); i++) {
-			for (size_t  j = 0; j < res2List.size(); j++) {
-
-				if (res1List.at(i).second == res2List.at(j).first) {
-					temp1.emplace(res1List.at(i));
-					temp2.emplace(res2List.at(j));
-				}
-			}
-		}
-
-	} else if (res1Syn2 == res2Syn2) {
-		resultsModifiedFlag = true;
-		for (size_t  i = 0; i < res1List.size(); i++) {
-			for (size_t  j = 0; j < res2List.size(); j++) {
-
-				if (res1List.at(i).second == res2List.at(j).second) {
-					temp1.emplace(res1List.at(i));
-					temp2.emplace(res2List.at(j));
-				}
-			}
-		}
-
-	} else {
-		// error
-	}
-
-	if (resultsModifiedFlag) {
-		res1.setPairResult(vector<pair<string, string>>());
-		res2.setPairResult(vector<pair<string, string>>());
-
-		for (set<pair<string, string>>::iterator iter = temp1.begin(); iter != temp1.end(); iter++) {
-			pair<string, string> p = *iter;
-			res1.addPairResult(p.first, p.second);
-		}
-
-		for (set<pair<string, string>>::iterator iter = temp2.begin(); iter != temp2.end(); iter++) {
-			pair<string, string> p = *iter;
-			res2.addPairResult(p.first, p.second);
-		}
-	}
-}
-
-void Results::getIntersect(Results &res) {
-	int nSynRes1 = this->getNumOfSyn();
-	int nSynRes2 = res.getNumOfSyn();
-
-	if (nSynRes1 == 1 && nSynRes2 == 1) {
-		getSingleIntercept(*this, res);
-
-	} else if (nSynRes1 == 1 && nSynRes2 == 2) {
-		getSinglePairIntercept(*this, res);
-
-	} else if (nSynRes1 == 2 && nSynRes2 == 1) {
-		res.getIntersect(*this);
-
-	} else if (nSynRes1 == 2 && nSynRes1 == 2) {
-		getPairIntercept(*this, res);
-
-	} else {
-		// error
-	}
-}
-
-set<string> Results::getSelectSynResult(string syn) {
-	string syn1 = this->getFirstClauseSyn();
-	string syn2 = this->getSecondClauseSyn();
-	int numSyn = this->getNumOfSyn();
-	set<string>* results = new set<string>();
-	
-	if (numSyn == 1 && syn == syn1) {
-		vector<string> singleList = this->getSinglesResults();
-		for (vector<string>::iterator iter = singleList.begin(); iter != singleList.end(); iter++) {
-			results->insert(*iter);
-		}
-		return *results;
-
-	} else if (numSyn == 2 && syn == syn1) {
-		vector<pair<string, string>> pairList = this->getPairResults();
-		for (size_t  i = 0; i < pairList.size(); i++) {
-			pair<string, string> p = pairList.at(i);
-			results->insert(p.first);
-		}
-		return *results;
-
-	} else if (numSyn == 2 && syn == syn2) {
-		vector<pair<string, string>> pairList = this->getPairResults();
-		for (size_t  i = 0; i < pairList.size(); i++) {
-			pair<string, string> p = pairList.at(i);
-			results->insert(p.second);
-		}
-		return *results;
-
-	} else {
-		return *results;
-	}
-}
-
-bool Results::usesSyn(string syn) {
-	int nSyn = this->getNumOfSyn();
-	string syn1 = this->getFirstClauseSyn();
-	string syn2 = this->getSecondClauseSyn();
-
-	if (nSyn == 1 && syn1 == syn) {
-		return true;
-
-	} else if (nSyn == 2 && syn1 == syn) {
-		return true;
-	
-	} else if (nSyn == 2 && syn2 == syn) {
+This is a helper function used by hasResults.
+It checks if resultsTable contains any results.
+*/
+bool Results::isResultsTableEmpty() {
+	if (this->resultsTable.empty() || 
+		this->resultsTable.at(0).empty()) {
 		return true;
 	} else {
 		return false;
 	}
 }
+
+/*
+This function checks if the synonym contains any results.
 */
+bool Results::hasResults(string syn) {
+	if (!isResultsTableEmpty()) {
+		unordered_map<string, string> row = this->resultsTable.at(0);
+		string value = row[syn];
+		
+		if (value.length() > 0) {
+			return true;
+		} else {
+			return false;
+		}
+
+	} else {
+		return false;
+	}
+}
+
+
+/*
+This function returns paired results. If results for a synonym in 
+the list of synonyms in synList does not exist, results returned will
+not contain missing synonym's results. Hence, check for the synonym's
+existance before using select functions.
+*/
+vector<unordered_map<string, string>>* Results::selectMultiSyn(vector<string> synList) {
+	unordered_map<string, string> row;
+	unordered_map<string, string> rowResults;
+	set<unordered_map<string, string>> uniqueValues;
+	vector<unordered_map<string, string>>* results = new vector<unordered_map<string, string>>();
+	
+	for (vector<unordered_map<string, string>>::iterator i = resultsTable.begin(); i != resultsTable.end(); ++i) {
+		row = *i;
+		rowResults = getSynResults(row, synList);	
+
+		if (!rowResults.empty()) {
+			uniqueValues.insert(rowResults);
+		}
+	}
+
+	for (set<unordered_map<string, string>>::iterator i = uniqueValues.begin(); i != uniqueValues.end(); ++i) {
+		row = *i;
+		results->push_back(row);
+	}
+
+	return results;
+}
+
+/*
+This function returns results paired with synonym syn. Results returned
+are distinct. If there are no results associated with that synonym,
+results returned will be an empty vector.
+*/
+vector<string>* Results::selectSyn(string syn) {
+	unordered_map<string, string> row;
+	string value;
+	set<string> uniqueValues;
+	vector<string>* results = new vector<string>();
+
+	for (vector<unordered_map<string, string>>::iterator i = resultsTable.begin(); i != resultsTable.end(); ++i) {
+		row = *i;
+		value = row[syn];
+		
+		if (value.length() > 0) {
+			uniqueValues.insert(value);
+		}
+	}
+
+	for (set<string>::iterator i = uniqueValues.begin(); i != uniqueValues.end(); i++) {
+		value = *i;
+		results->push_back(value);
+	}
+
+	return results;
+}
+
+/*
+This function gets results row that are associated with the following
+synonym and value from the vector of unordered_map it is provided with.
+*/
+vector<unordered_map<string, string>>* Results::selectSynWhere(string syn, string val, vector<unordered_map<string, string>> res) {
+	vector<unordered_map<string, string>>* results = new vector<unordered_map<string, string>>();
+	if (syn.length() > 0 && val.length() > 0) {
+		unordered_map<string, string> row;
+		string value;
+
+		for (vector<unordered_map<string, string>>::iterator i = res.begin(); i != res.end(); ++i) {
+			row = *i;
+			value = row[syn];
+			if (value == val) {
+				results->push_back(row);
+			}
+		}
+
+	} 
+	
+	return results;
+}
+
+/*
+When there are results in insertStack, insertBufferFlag will
+be true.
+*/
+void Results::setInsertBufferFlag() {
+	insertBufferFlag = true;
+}
+
+/*
+When the results object is returned to the evaluator, the
+insert flag will be reset
+*/
+void Results::resetInsertBufferFlag() {
+	insertBufferFlag = false;
+}
+
+/*
+This function inserts the value of the given list of synonyms into 
+the resultsTable. It does not require the synonyms to exist beforehand
+for the insertion to work. It acts as a buffer and
+requires the function pushChanges to enter results into the resultsTable.
+*/
+bool Results::insertMultiResult(unordered_map<string, string> results) {
+	if (results.size() > 0) {
+		insertStack.insert(results);
+		setInsertBufferFlag();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/*
+This function inserts the value of the single given synonym into 
+the resultsTable. It does not require the synonym to exist beforehand
+for the insertion to work. It acts as a buffer and
+requires the function pushChanges to enter results into the resultsTable.
+*/
+bool Results::insertResult(string syn, string value) {
+	if (syn.length() > 0 && value.length() > 0) {
+		unordered_map<string, string> result;
+		result[syn] = value;
+		insertStack.insert(result);
+		setInsertBufferFlag();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/*
+This is a helper function to push inserted results into resultsTable.
+It is called by pushChanges
+*/
+bool Results::pushInsertResults() {
+	unordered_map<string, string> row;
+	unordered_map<string, string> stackRow;
+	string key;
+	string value;
+	int initialSize = resultsTable.size();
+		
+	for (int i = 0; i < initialSize; ++i) {
+		row = resultsTable.at(i);
+		for (set<unordered_map<string, string>>::iterator j = insertStack.begin(); j != insertStack.end(); ++j) {
+			stackRow = *j;
+			for (unordered_map<string, string>::iterator k = stackRow.begin(); k != stackRow.end(); ++k) {
+				key = k->first;
+				value = k->second;
+				row[key] = value;
+			}
+
+			resultsTable.push_back(row);
+		}
+	}
+
+	resultsTable.erase(resultsTable.begin(), resultsTable.begin() + initialSize);
+	return true;
+}
+
+/*
+This is a helper function to push deleted results changes into resultsTable.
+It is called by pushChanges
+*/
+bool Results::pushDelResults() {
+	unordered_map<string, string> row;
+	unordered_map<string, string> stackRow;
+	vector<int> rowNumber;
+	int position;
+	string key;
+	string value;
+	string resultValue;
+
+	for (vector<unordered_map<string, string>>::iterator i = resultsTable.begin(); i != resultsTable.end(); ++i) {
+		row = *i;
+		for (set<unordered_map<string, string>>::iterator j = delStack.begin(); j != delStack.end(); ++j) {
+			stackRow = *j;
+			for (unordered_map<string, string>::iterator k = stackRow.begin(); k != stackRow.end(); ++k) {
+				key = k->first;
+				value = k->second;
+				resultValue = row[key];
+
+				if (resultValue == value) {
+					position = i - resultsTable.begin();
+					rowNumber.push_back(position);
+				}
+			}
+		}
+	}
+
+	for (vector<int>::iterator i = rowNumber.begin(); i != rowNumber.end(); ++i) {
+		resultsTable.erase(resultsTable.begin() + *i);
+	}
+
+	return true;
+}
+
+/*
+This function is called after insertResult or insertMultiResult.
+It enters the results into the resultsTable.
+*/
+bool Results::pushChanges() {
+	if (insertBufferFlag) {
+		pushInsertResults();
+		return true;
+	} else if (delBufferFlag) {
+		pushDelResults();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/*
+When there are results in delStack, delBufferFlag will
+be true.
+*/
+void Results::setDelBufferFlag() {
+	delBufferFlag = true;
+}
+
+/*
+When the results object is returned to the evaluator, the
+delete flag will be reset
+*/
+void Results::resetDelBufferFlag() {
+	delBufferFlag = false;
+}
+
+/*
+This function deletes the associated results of the given list of synonyms in 
+the resultsTable. It does not require the synonyms to exist beforehand
+for the deletion to work. It acts as a buffer and
+requires the function pushChanges to enter results into the resultsTable.
+*/
+bool Results::delMultiResult(unordered_map<string, string> results) {
+	if (results.size() > 0) {
+		delStack.insert(results);
+		setDelBufferFlag();
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/*
+This function deletes the value of the single given synonym in 
+the resultsTable. It does not require the synonym to exist beforehand
+for the deletion to work. It acts as a buffer and
+requires the function pushChanges to enter results into the resultsTable.
+*/
+bool Results::delResult(string syn, string value) {
+	if (syn.length() > 0 && value.length() > 0) {
+		unordered_map<string, string> result;
+		result[syn] = value;
+		delStack.insert(result);
+		setDelBufferFlag();
+		return true;
+	} else {
+		return false;
+	}
+}
--- SPA/Results.h
+++ SPA/Results.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <set>
 #include <unordered_map>
 
 using namespace std;
@@ -15,20 +16,37 @@ public:
 	void setClausePass();
 	void resetClausePass();
 
-	vector<bool> isExist(vector<string> synList);
-	vector<unordered_map<string, string>> selectMultiSyn(vector<string> synList); // we are using database query language as a guide, hence select instead of get
-	vector<string> selectSyn(string syn);
-	vector<unordered_map<string, string>> selectSynWhere(string syn, string val, vector<unordered_map<string, string>>);
+	bool hasResults(string syn);
+
+	vector<unordered_map<string, string>>* selectMultiSyn(vector<string> synList); // we are using database query language as a guide, hence select instead of get
+	vector<string>* selectSyn(string syn);
+	vector<unordered_map<string, string>>* selectSynWhere(string syn, string val, vector<unordered_map<string, string>> results);
 	
 	// Insert will help create the synonym and add results to it
 	bool insertMultiResult(unordered_map<string, string> results);
 	bool insertResult(string syn, string value);
+	bool pushChanges();
 
 	bool delMultiResult(unordered_map<string, string> results);
 	bool delResult(string syn, string value);
 
 private:
 	static bool clausePassed;
-	static vector<unordered_map<string, string>> resultsTable;
+	static bool insertBufferFlag;
+	static bool delBufferFlag;
+
+	vector<unordered_map<string, string>> resultsTable;
+	set<unordered_map<string, string>> insertStack;
+	set<unordered_map<string, string>> delStack;
+
+	bool isResultsTableEmpty();
+	void setInsertBufferFlag();
+	void resetInsertBufferFlag();
+	void setDelBufferFlag();
+	void resetDelBufferFlag();
+	bool pushInsertResults();
+	bool pushDelResults();
+
+	unordered_map<string, string> getSynResults(unordered_map<string, string> row, vector<string> synList);
 };
 
