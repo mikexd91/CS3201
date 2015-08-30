c0a7b0da1862ab057f4c71c47d967e7428b48ca9
Andrew Low <andrewjrlow@gmail.com>
2015-08-31 01:55:41 +0800

    unit tests for q parser
    FEATURES TO ADD: change parser variable settings from string const to node types (check for synonyms, fixed vars and wildcards)

--- UnitTesting/QueryParserTest.h
+++ UnitTesting/QueryParserTest.h
@@ -9,8 +9,8 @@ class QueryParserTest : public CPPUNIT_NS::TestFixture{
 	CPPUNIT_TEST( testDeclaration );
 	CPPUNIT_TEST( testSelect );
 	CPPUNIT_TEST( testClause );
-	//CPPUNIT_TEST( testPattern );
-	//CPPUNIT_TEST ( testParser );
+	CPPUNIT_TEST( testPattern );
+	CPPUNIT_TEST ( testParser );
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -22,6 +22,6 @@ public:
 	void testDeclaration();
 	void testSelect();
 	void testClause();
-	//void testPattern();
-	//void testParser();
+	void testPattern();
+	void testParser();
 };
