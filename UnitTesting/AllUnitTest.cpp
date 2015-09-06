ead9a0b2b8022b9927515e9c8859f9d7b2a1496b
Peining <peining_pan@hotmail.com>
2015-09-06 16:20:16 +0800

    Fix bugs clauses and build

--- UnitTesting/AllUnitTest.cpp
+++ UnitTesting/AllUnitTest.cpp
@@ -37,6 +37,7 @@ unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("FollowsClauseTest"
 unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("FollowsStarClauseTest").makeTest());
 unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("UsesClauseTest").makeTest());
 */
+unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ParentClauseTest").makeTest());
 // PQL Evaluator
 unitSuite->addTest(CppUnit::TestFactoryRegistry::getRegistry("ResultsTest").makeTest());
 
