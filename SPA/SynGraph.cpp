53e0c9c708e9e0838daf895148a91059bd90b11c
heyjiawei <jaewei.j.w@gmail.com>
2015-10-25 23:53:59 +0800

    SynGraph class done. Moving on to testing

--- SPA/SynGraph.cpp
+++ SPA/SynGraph.cpp
@@ -3,128 +3,125 @@
 using namespace stringconst;
 
 SynGraph::SynGraph(void) {
-	synTable = unordered_map<string, SynNode>();
-	graphTable = unordered_map<string, EdgeList>();
-	vector<int> componentWeightList = vector<int>();
-	vector<int> componentOrder = vector<int>();
-	vector<int> orderedClauseList = vector<int>();
+	synTable = SynTable();
+	graphTable = GraphTable();
+	compClauseMap = ComponentMap();
+	compWeightList = ComponentWeight();
 
-	fixedCount = 1;
-	componentIndex = 1;
+	componentIndex = 0;
 }
 
 SynGraph::~SynGraph(void) {
 
 }
 
-void SynGraph::reorderQuery(Query* q) {
+Query* SynGraph::reorderQuery(Query* q) {
 	originalClauseList = q->getClauseList();
 	populateGraphTable(q);
 	setComponents();
-	orderComponents();
-	orderComponentClauses();
 
 	// get component index, starting from smallest weighted component
 	// set q with this component's clauseList
 	// do the same for the other components
-
+	buildQuery(q);
 	// after testing, add in empty clauses for results optimisation
+	return q;
 }
 
+void SynGraph::buildQuery(Query* q) {
+	// order component in ascending weight order
+	std::sort(compWeightList.begin(), compWeightList.end(), componentCmp());
+	int clauseIndex = 0;
+	vector<Clause*> modifiedClause = vector<Clause*>();
+	vector<Clause*>::iterator iter = modifiedClause.begin();
+	BOOST_FOREACH(auto i, compWeightList) {
+		int componentNum = i.first;
+		vector<Clause*> clauselist = compClauseMap[componentNum];
+		modifiedClause.insert(iter + clauselist.size(), clauselist.begin(), clauselist.end());
+		iter += clauselist.size();
+	}
+	q->setClauseList(modifiedClause);
+}
+
+// Populates synTable and graphTable
 void SynGraph::populateGraphTable(Query* q) {
 	createSelectSynNodes(q);
 	createClauseSynNodes(q);
 }
 
-void SynGraph::setComponents() {
-	BOOST_FOREACH(auto i, synTable) {
-		string syn = i.first;
-		SynNode node1 = i.second;
-		bool componentNotSet = (node1.getComponentNum() == -1);
-		if (componentNotSet) {
-			unordered_set<pair<int, int>> clauseIndexSet = dfs(node1, syn, componentIndex);
-			vector<Clause*> clauseList = setClauseWeight(clauseIndexSet);
-			orderClauses(clauseList);
-			componentClause.insert(make_pair(componentIndex, clauseList));
-			componentIndex++;
+void SynGraph::createSelectSynNodes(Query* q) {
+	vector<StringPair> selectList = q->getSelectList();
+	bool hasSelectSyn = (selectList.at(0).getSecond() != ARG_BOOLEAN);
+	// Case of "Select a s.t. ... "
+	if (hasSelectSyn) {
+		BOOST_FOREACH(auto i, selectList) {
+			// Create and set SynNode
+			// Select synonyms have a weight of 5
+			// and component number -1 to define 
+			// no set component yet
+
+			string syn = i.getFirst();
+			SynNode synNode = SynNode();
+			synNode.setSynonym(syn);
+			synNode.setWeight(5);
+			synNode.setComponentNum(-1);
+
+			// Set tables
+			synTable.insert(make_pair(syn, synNode));
+			graphTable.insert(make_pair(syn, EdgeList())); // Select synonyms have no Edges
 		}
 	}
+	// Case of "Select BOOLEAN" will not have a SynNode
 }
 
-void SynGraph::orderClauses(vector<Clause*> clauseList) {
-	std::sort(clauseList.begin(), clauseList.end(), clauseCmp());
-}
+void SynGraph::createClauseSynNodes(Query* q) {
+	vector<Clause*> clauseList = q->getClauseList();
+	int clauseIndex = 0;
+	int fixedCount = 0;
+	BOOST_FOREACH(auto i, clauseList) {
+		Clause* c = i;
+		ClauseType cType = i->getClauseType();
+		vector<string> synList = getClauseSynonym(c, cType);
 
-vector<Clause*> SynGraph::setClauseWeight(unordered_set<pair<int, int>> clauseSet) {
-	vector<Clause*> clauseList = vector<Clause*>();
-	BOOST_FOREACH(auto i, clauseSet) {
-		Clause* c = originalClauseList.at(i.first);
-		c->setWeight(i.second);
-		clauseList.push_back(c);
-	}
-	return clauseList;
-}
+		// Clause returns a boolean
+		if (synList.size() == 0) {
+			createFixedArgNode(c, clauseIndex, fixedCount);
+			fixedCount++;
 
-void SynGraph::orderComponents() {
-	std::sort(componentWeightList.begin(), componentWeightList.end(), componentCmp());
-}
-
-void SynGraph::orderComponentClauses() {
-	 vector <bool> componentChecked (componentIndex);
-	 vector<int> clauseIndexOrdered = vector<int>();
-
-}
-
-// Helper function for setComponents() START
-unordered_set<pair<int, int>> SynGraph::dfs(SynNode node1, string syn, int index) {
-	unordered_set<pair<int, int>> clauseSet = unordered_set<pair<int, int>>();
-	int componentWeight = 0;
-	queue<SynNode> q;
-	q.push(node1);
-	componentWeight += node1.getWeight();
-
-	while (q.size() > 0) {
-		SynNode node = q.front();
-		q.pop();
-		if (node.getComponentNum() == -1) {
-			node.setComponentNum(index);
-			string nodeName = node.getSynonym();
-			EdgeList edgelist = graphTable[nodeName];
-			
-			// In the case of select synonym, it might not
-			// have any edge in it's edgelist yet
-			BOOST_FOREACH(auto i, edgelist) {
-				SynEdge edge = i.first;
-				SynNode neighbour = i.second;
-				componentWeight += edge.getWeight();
-
-				//  check if it is another synonym
-				if (!neighbour.isSame(node)) {
-					// neighbour is another synonym,
-					// this clause has 2 synonyms
-					q.push(neighbour);
-					
-					// Add clause and clause weight to clauseSet
-					int clauseWeight = edge.getWeight() + 
-									node.getWeight() + 
-									neighbour.getWeight();
-					clauseSet.insert(make_pair(edge.getClauseIndex(), clauseWeight));
-
-				} else {
-					// neighbour is me, this clause only has 1 synonym
-					int clauseWeight = edge.getWeight() + node.getWeight();
-					clauseSet.insert(make_pair(edge.getClauseIndex(), clauseWeight));
-				}
-			}
+		} else { // Clause has synonym(s)
+			createUnfixedArgNode(c, clauseIndex, synList);
 		}
+		clauseIndex++;
 	}
-	componentWeightList.push_back(make_pair(index, componentWeight));
-	return clauseSet;
 }
 
-// Helper function for setComponent() END //
+// Retrieves the synonym arguments in a Clause
+vector<string> SynGraph::getClauseSynonym(Clause* c, ClauseType cType) {
+	vector<string> synList = vector<string>();
+	bool isSuchThatClauseType = (cType != WITH_) && 
+		(cType != PATTERNASSG_) &&
+		(cType != PATTERNIF_) &&
+		(cType != PATTERNWHILE_);
 
-// Helper functions for populateGraphTable START //
+	bool isPatternClauseType = (cType == PATTERNASSG_) ||
+		(cType == PATTERNIF_) ||
+		(cType == PATTERNWHILE_);
+
+	if (isSuchThatClauseType) {
+		SuchThatClause* stc = (SuchThatClause*) c;
+		synList = getSynFromSuchThatClause(stc);
+	
+	} else if (isPatternClauseType) {
+		PatternClause* pc = (PatternClause*) c;
+		synList = getSynFromPatternClause(pc);
+
+	} else { // If is a With clause
+		WithClause* wc = (WithClause*) c;
+		synList = getSynFromWithClause(wc);
+	}
+	return synList;
+}
+
 vector<string> SynGraph::getSynFromSuchThatClause(SuchThatClause* stc) {
 	bool isFirstArgSyn = ((stc->getFirstArgFixed()) == false) && 
 						(stc->getFirstArgType() != ARG_GENERIC);
@@ -158,42 +155,17 @@ vector<string> SynGraph::getSynFromWithClause(WithClause* wc) {
 	return vector<string>();
 }
 
-vector<string> SynGraph::getClauseSynonym(Clause* c, ClauseType cType) {
-	vector<string> synList = vector<string>();
-	bool isSuchThatClauseType = (cType != WITH_) && 
-		(cType != PATTERNASSG_) &&
-		(cType != PATTERNIF_) &&
-		(cType != PATTERNWHILE_);
-
-	bool isPatternClauseType = (cType == PATTERNASSG_) ||
-		(cType == PATTERNIF_) ||
-		(cType == PATTERNWHILE_);
-
-	if (isSuchThatClauseType) {
-		SuchThatClause* stc = (SuchThatClause*) c;
-		synList = getSynFromSuchThatClause(stc);
-	
-	} else if (isPatternClauseType) {
-		PatternClause* pc = (PatternClause*) c;
-		synList = getSynFromPatternClause(pc);
-
-	} else { // If is a With clause
-		WithClause* wc = (WithClause*) c;
-		synList = getSynFromWithClause(wc);
-	}
-	return synList;
-}
-
-void SynGraph::createFixedArgNode(Clause* c, int cIndex) {
+// Creates and set fixed arg SynNodes and 
+// add it to componentMap and componentWeight
+void SynGraph::createFixedArgNode(Clause* c, int cIndex, int fixedCount) {
 	// Create and set SynNode
 	// Fixed arg have a weight of 0
+	// and they are a component by themselves
 	string syn = boost::lexical_cast<string>(fixedCount);
 	SynNode synNode = SynNode();
 	synNode.setSynonym(syn);
 	synNode.setWeight(0);
 	synNode.setComponentNum(componentIndex);
-	fixedCount++;
-	componentIndex++;
 
 	// Create SynEdge and link to itself
 	SynEdge edge = SynEdge(c->getWeight(), cIndex);
@@ -203,6 +175,36 @@ void SynGraph::createFixedArgNode(Clause* c, int cIndex) {
 	// Insert into tables
 	synTable.insert(make_pair(syn, synNode));
 	graphTable.insert(make_pair(syn, edgeList));
+
+	// Add to componentMap and componentWeight
+	storeFixedArgClauseCompWeight(componentIndex, syn);
+	vector<Clause*> clauseList = edgeToClause(syn);
+	compClauseMap[componentIndex] = clauseList;
+	componentIndex++;
+}
+
+// As a fixed clause is a component on it's own, this function
+// stores the mapping of the weight of a fixed clause component to 
+// the component number
+void SynGraph::storeFixedArgClauseCompWeight(int index, string syn) {
+	int weight = 0;
+	SynNode node = synTable[syn];
+	weight += node.getWeight();
+
+	EdgeList edgelist = graphTable[syn];
+	SynEdge edge = edgelist.at(0).first;
+	weight += edge.getWeight();
+
+	compWeightList.push_back(make_pair(index, weight));
+}
+
+vector<Clause*> SynGraph::edgeToClause(string syn) {
+	EdgeList edgelist = graphTable[syn];
+	SynEdge edge = edgelist.at(0).first;
+	Clause* c = originalClauseList.at(edge.getClauseIndex());
+	vector<Clause*> clauselist = vector<Clause*>();
+	clauselist.push_back(c);
+	return clauselist;
 }
 
 void SynGraph::linkSingleUnfixedArgNode(Clause* c, int cIndex, string syn) {
@@ -307,53 +309,93 @@ void SynGraph::createUnfixedArgNode(Clause* c, int cIndex,
 	}
 }
 
-void SynGraph::createClauseSynNodes(Query* q) {
-	vector<Clause*> clauseList = q->getClauseList();
-	int clauseIndex = 0;
-	BOOST_FOREACH(auto i, clauseList) {
-		Clause* c = i;
-		ClauseType cType = i->getClauseType();
-		vector<string> synList = getClauseSynonym(c, cType);
 
-		// Clause returns a boolean
-		if (synList.size() == 0) {
-			createFixedArgNode(c, clauseIndex);
 
-		} else { // Clause needs to populate a synonym
-			createUnfixedArgNode(c, clauseIndex, synList);
-		}
-		clauseIndex++;
-	}
-}
 
-bool SynGraph::isSelectSyn(vector<StringPair> selectList) {
-	if (selectList.at(0).getSecond() != ARG_BOOLEAN) {
-		return true;
-	} else {
-		return false;
-	}
-}
 
-void SynGraph::createSelectSynNodes(Query* q) {
-	vector<StringPair> selectList = q->getSelectList();
-	bool hasSelectSyn = isSelectSyn(selectList);
-	// Case of "Select a s.t. ... "
-	if (hasSelectSyn) {
-		BOOST_FOREACH(auto i, selectList) {
-			// Create and set SynNode
-			// Select synonyms have a weight of 5
-			string syn = i.getFirst();
-			SynNode synNode = SynNode();
-			synNode.setSynonym(syn);
-			synNode.setWeight(5);
-			synNode.setComponentNum(-1);
 
-			// Set tables
-			synTable.insert(make_pair(syn, synNode));
-			graphTable.insert(make_pair(syn, EdgeList())); // Select synonyms have no Edges
+
+
+
+
+// Set the component number of synNodes that do not have a component
+// number yet. SynNodes that can be reached from another synNode are
+// in the same component.
+void SynGraph::setComponents() {
+	BOOST_FOREACH(auto i, synTable) {
+		string syn = i.first;
+		SynNode node1 = i.second;
+		bool componentNotSet = (node1.getComponentNum() == -1);
+		if (componentNotSet) {
+			// get clauses and their weight from traversing the graph
+			// set the clauses with their respective weight
+			// sort clauselist (component's clauses) by ascending weight order
+			// map componentIndex to clauselist
+			ClauseWeight clauseWeightMatchList = setComponentNumByDFS(node1, syn, componentIndex);
+			vector<Clause*> clauseList = setClauseWeight(clauseWeightMatchList);
+			std::sort(clauseList.begin(), clauseList.end(), clauseCmp());
+			compClauseMap[componentIndex] = clauseList;
+			componentIndex++;
 		}
 	}
-	// Case of "Select BOOLEAN" will not have a SynNode
 }
 
-// Helper functions for populateGraphTable END //
+vector<Clause*> SynGraph::setClauseWeight(ClauseWeight clauseSet) {
+	vector<Clause*> clauseList = vector<Clause*>();
+	BOOST_FOREACH(auto i, clauseSet) {
+		Clause* c = originalClauseList.at(i.first);
+		c->setWeight(i.second);
+		clauseList.push_back(c);
+	}
+	return clauseList;
+}
+
+// This function sets the componentIndex of the SynNode as it traverse the graph.
+// It also sets the component's weight at the end of traversal
+SynGraph::ClauseWeight SynGraph::setComponentNumByDFS(SynNode node1, string syn, int index) {
+	ClauseWeight clauseSet = ClauseWeight();
+	int componentWeight = 0;
+	queue<SynNode> q;
+	q.push(node1);
+	componentWeight += node1.getWeight();
+
+	while (q.size() > 0) {
+		SynNode node = q.front();
+		q.pop();
+		if (node.getComponentNum() == -1) {
+			node.setComponentNum(index);
+			string nodeName = node.getSynonym();
+			EdgeList edgelist = graphTable[nodeName];
+			
+			// In the case of select synonym, it might not
+			// have any edge in it's edgelist yet
+			BOOST_FOREACH(auto i, edgelist) {
+				SynEdge edge = i.first;
+				SynNode neighbour = i.second;
+				componentWeight += edge.getWeight();
+
+				//  check if it is another synonym
+				if (!neighbour.isSame(node)) {
+					// neighbour is another synonym,
+					// this clause has 2 synonyms
+					q.push(neighbour);
+					
+					// Add clause and clause weight to clauseSet
+					int clauseWeight = edge.getWeight() + 
+									node.getWeight() + 
+									neighbour.getWeight();
+					clauseSet.insert(make_pair(edge.getClauseIndex(), clauseWeight));
+
+				} else {
+					// neighbour is me, this clause only has 1 synonym
+					int clauseWeight = edge.getWeight() + node.getWeight();
+					clauseSet.insert(make_pair(edge.getClauseIndex(), clauseWeight));
+				}
+			}
+		}
+	}
+	compWeightList.push_back(make_pair(index, componentWeight));
+	return clauseSet;
+}
+
+// Helper function for setComponent() END //
--- SPA/SynGraph.h
+++ SPA/SynGraph.h
@@ -51,63 +51,46 @@ struct clauseCmp
 class SynGraph
 {
 public:
+	// For pairs, first of type int always refer to index, 
+	// second int type refers to what I am storing together 
+	// with that index
 	typedef vector<pair<SynEdge, SynNode>> EdgeList;
+	typedef unordered_map<string, SynNode> SynTable;
+	typedef unordered_map<string, EdgeList> GraphTable;
+	typedef unordered_map<int, vector<Clause*>> ComponentMap;
+	typedef vector<pair<int, int>> ComponentWeight;
+	typedef unordered_set<pair<int, int>> ClauseWeight;
 
 	SynGraph(void);
 	~SynGraph(void);
 
-	void reorderQuery(Query*);
+	Query* reorderQuery(Query*); 		// Reorders the query for optimisation
+	//Query* optimiseForResult(Query*);	// Add in empty clauses to denote the start of a component
 
 private:
-	unordered_map<string, SynNode> synTable;
-	unordered_map<string, EdgeList> graphTable;
-	vector<pair<int, int>> componentWeightList;
-	set<pair<int, vector<Clause*>>> componentClause;
-	vector<int> componentOrder;
-	vector<int> orderedClauseList;
 	vector<Clause*> originalClauseList;
+	SynTable synTable;
+	GraphTable graphTable;
+	ComponentMap compClauseMap;			// Stores the component index and the ClauseList under it
+	ComponentWeight compWeightList;		// Stores the component index and the component's weight
 
-	int fixedCount;
 	int componentIndex;
 
-	// Create Synonyms, create Edges, link synonyms
-	// Fill in graphTable
+	void buildQuery(Query*);
 	void populateGraphTable(Query*);
-
-	// populateGraphTable helper functions:
 	void createSelectSynNodes(Query*);
-	bool isSelectSyn(vector<StringPair>);
 	void createClauseSynNodes(Query*);
 	vector<string> getClauseSynonym(Clause*, ClauseType);
 	vector<string> getSynFromSuchThatClause(SuchThatClause*);
 	vector<string> getSynFromPatternClause(PatternClause*);
 	vector<string> getSynFromWithClause(WithClause*);
-	void createFixedArgNode(Clause*, int);
-	void createUnfixedArgNode(Clause*, int, vector<string>); // Generics are handled here
+	void createFixedArgNode(Clause*, int, int);
+	void storeFixedArgClauseCompWeight(int, string);
+	vector<Clause*> edgeToClause(string);
 	void linkSingleUnfixedArgNode(Clause*, int, string);
-
-	// Find disjointed sets, set componentIndex
+	void createUnfixedArgNode(Clause*, int, vector<string>);
 	void setComponents(void);
-	unordered_set<pair<int, int>> dfs(SynNode, string, int);
-	vector<Clause*> setClauseWeight(unordered_set<pair<int, int>>);
-	void orderClauses(vector<Clause*>);
-	void orderComponents(void);
-	void orderComponentClauses(void);
-
-	/*
-	bool isSelectSyn(vector<StringPair>);
+	vector<Clause*> setClauseWeight(ClauseWeight);
+	ClauseWeight setComponentNumByDFS(SynNode, string, int);
 	
-	void createSelectSynNodes(Query*);
-	void createClauseSynNodes(Query*);
-	void createFixedArgSynNode(Clause*, int);
-	void createUnfixedArgSynNode(Clause*, int, vector<string>);
-	void findDisjointedComponents(void);
-
-	vector<string> getSynonyms(Clause*);
-	vector<string> getSynFromPatternClause(PatternClause*);
-	vector<string> getSynFromSuchThatClause(SuchThatClause*);
-	*/
-	//vector<string> getSelectSyn(Query);
-	//void createSynNodes(vector<string>, vector<Clause*>);
-	//void buildGraph(vector<string>, vector<Clause*>);
 };
