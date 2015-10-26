948344d8c4028f17f167a4b0bb66a7844e26b391
heyjiawei <jaewei.j.w@gmail.com>
2015-10-26 17:00:03 +0800

    setting up unit testing for synGraph

--- UnitTesting/SynGraphTest.cpp
+++ UnitTesting/SynGraphTest.cpp
@@ -1,4 +1,10 @@
+#include <cppunit/config/SourcePrefix.h>
 #include "SynGraphTest.h"
+#include "../SPA/SynGraph.h"
+#include "../SPA/Clause.h"
+#include "../SPA/Utils.h"
+
+using namespace stringconst;
 
 void SynGraphTest::setUp() {
 }
@@ -9,5 +15,10 @@ void SynGraphTest::tearDown() {
 CPPUNIT_TEST_SUITE_REGISTRATION( SynGraphTest );
 
 void SynGraphTest::testPopulateGraphTableSelectBoolNoClause() {
+	SynGraph *graph = new SynGraph();
+
+	StringPair *p = new StringPair();
+	p->setFirst("BOOLEAN");
+	p->setSecond(ARG_BOOLEAN);
 
 }
