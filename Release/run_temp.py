859dd293d8d0efb067fd6c7a85bf2bbec721e27a
Jonathan Chiam <jczjchiam@gmail.com>
2015-10-31 15:03:01 +0800

    Follows-ss2 done

--- Release/run_temp.py
+++ Release/run_temp.py
@@ -11,6 +11,7 @@ import os
 # os.system("AutoTester SystemTests/Sources/System-Source-2.txt SystemTests/Parent-ss2.txt SystemTests/Results/parent-ss2.xml")
 # os.system("AutoTester SystemTests/Sources/System-Source-1.txt SystemTests/ParentStar-ss1.txt SystemTests/Results/parentstar-ss1.xml")
 # os.system("AutoTester SystemTests/Sources/System-Source-1.txt SystemTests/Follows-ss1.txt SystemTests/Results/follows-ss1.xml")
+# os.system("AutoTester SystemTests/Sources/System-Source-2.txt SystemTests/Follows-ss2.txt SystemTests/Results/follows-ss2.xml")
 # os.system("AutoTester SystemTests/Sources/System-Source-1.txt SystemTests/FollowsStar-ss1.txt SystemTests/Results/followsstar-ss1.xml")
 # os.system("AutoTester SystemTests/Sources/System-Source-2.txt SystemTests/Calls-ss2.txt SystemTests/Results/calls-ss2.xml")
 # os.system("AutoTester SystemTests/Sources/System-Source-2.txt SystemTests/CallsStar-ss2.txt SystemTests/Results/callsstar-ss2.xml")
