3de5b2bfdad579010c9c6831f4e11ebff831153c
Leon Chin <leonchin499@gmail.com>
2015-11-03 14:33:34 +0800

    done with nextbip. still working out algo for nextstarbip

---
+++ SPA/NextStarBipClause.cpp
@@ -0,0 +1,248 @@
+/*
+ * NextStarBipClause.cpp
+ *
+ *  Created on: 2 Nov, 2015
+ *      Author: Leon
+ */
+
+#include "NextStarBipClause.h"
+#include "boost/foreach.hpp"
+#include "boost/lexical_cast.hpp"
+
+using namespace stringconst;
+using namespace boost;
+
+NextStarBipClause::NextStarBipClause()
+	: SuchThatClause(NEXTSTARBIP_) {
+	stmtTable = StmtTable::getInstance();
+	cfg = CFGbip::getInstance();
+	match = false;
+}
+
+NextStarBipClause::~NextStarBipClause() {
+
+}
+
+bool NextStarBipClause::isValid(void) {
+	bool firstArg = (firstArgType == ARG_GENERIC) || (firstArgType == ARG_WHILE)
+			|| (firstArgType == ARG_PROGLINE) || (firstArgType == ARG_IF)
+			|| (firstArgType == ARG_STATEMENT) || (firstArgType == ARG_ASSIGN)
+			|| (firstArgType == ARG_CALL);
+	bool secondArg = (firstArgType == ARG_GENERIC) || (firstArgType == ARG_WHILE) 
+			|| (firstArgType == ARG_PROGLINE) || (firstArgType == ARG_IF) 
+			|| (firstArgType == ARG_STATEMENT) || (firstArgType == ARG_ASSIGN)
+			|| (firstArgType == ARG_CALL);
+
+	return firstArg && secondArg;
+}
+
+// NextBip*(4, 2)
+bool NextStarBipClause::evaluateS1FixedS2Fixed(string s1, string s2) {
+	return false;
+}
+
+// NextBip*(_, _)
+bool NextStarBipClause::evaluateS1GenericS2Generic() {
+	unordered_set<Statement*> stmts = stmtTable->getAllStmts();
+
+	BOOST_FOREACH(auto s, stmts) {
+		if(!s->getNextBip().empty()) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+// NextBip*(_, 2)
+bool NextStarBipClause::evaluateS1GenericS2Fixed(string s2) {
+	Statement* stmt = stmtTable->getStmtObj(atoi(s2.c_str()));
+
+	if(!stmt->getPrevBip().empty()) {
+		return true;
+	}
+
+	return false;
+}
+
+// NextBip*(2, _)
+
+bool NextStarBipClause::evaluateS1FixedS2Generic(string s1) {
+	Statement* stmt = stmtTable->getStmtObj(atoi(s1.c_str()));
+
+	if(!stmt->getNextBip().empty()) {
+		return true;
+	}
+
+	return false;
+}
+
+// NextBip*(3, s) || NextBip*(3, a) || NextBip*(3, c) || NextBip*(3, if) || NextBip*(3, w)
+unordered_set<string> NextStarBipClause::getAllS2WithS1Fixed(string s1) {
+	results.clear();
+	return results;
+}
+
+// NextBip*(_, s) || NextBip*(_, a) || NextBip*(_, c) || NextBip*(_, if) || NextBip*(_, w)
+unordered_set<string> NextStarBipClause::getAllS2() {
+	results.clear();
+	unordered_set<Statement*> stmts = getNeededStmts(secondArgType);
+
+	BOOST_FOREACH(auto s, stmts) {
+		if(!s->getPrevBip().empty()) {
+			results.insert(lexical_cast<string>(s->getStmtNum()));
+		}
+	}
+
+	return results;
+}
+
+// NextBip*(s, 3) || NextBip*(a, 3) || NextBip*(c, 3) || NextBip*(if, 3) || NextBip*(w, 3)
+unordered_set<string> NextStarBipClause::getAllS1WithS2Fixed(string s2) {
+	results.clear();
+	return results;
+}
+
+// NextBip*(s, _) || NextBip*(a, _) || NextBip*(c, _) || NextBip*(if, _) || NextBip*(w, 3)
+unordered_set<string> NextStarBipClause::getAllS1() {
+	results.clear();
+	unordered_set<Statement*> stmts = getNeededStmts(firstArgType);
+
+	BOOST_FOREACH(auto s, stmts) {
+		if(!s->getNextBip().empty()) {
+			results.insert(lexical_cast<string>(s->getStmtNum()));
+		}
+	}
+
+	return results;
+}
+
+unordered_set<vector<string>> NextStarBipClause::getAllS1AndS2() {
+	resultsPair.clear();
+	return resultsPair;
+}
+
+void NextStarBipClause::dfsFind(Statement* stmt, string str, vector<string> visited, stack<int> entrance) {
+	string currStmt = lexical_cast<string>(stmt->getStmtNum());
+	GNode* bipNode = stmt->getGBipNodeRef();
+
+	if(match) {
+		return;
+	}
+
+	if(currStmt == str) {
+		match = true;
+		return;
+	}
+
+	if(!contains(visited, currStmt)) {
+		visited.push_back(currStmt);
+
+		if(bipNode->getNodeType() == CALL_) {
+			ProcGNode* procChild = (ProcGNode*) bipNode->getChildren().at(0);
+			int indexOfEntrance = getNodePosition(procChild->getParents(), bipNode);
+			entrance.push(indexOfEntrance);
+			unordered_set<int> nextSet = stmt->getNextBip();
+			BOOST_FOREACH(auto next, nextSet) {
+				Statement* nextStmt = stmtTable->getStmtObj(next);
+				dfsFind(nextStmt, str, visited, entrance);
+			}
+		} else if(bipNode->getNodeType() == WHILE_) {
+			if(bipNode->getChildren().at(1)->getNodeType() == DUMMY_) {
+				int firstChildStmtNum = bipNode->getChildren().at(0)->getStartStmt();
+				dfsFind(stmtTable->getStmtObj(firstChildStmtNum), str, visited, entrance);
+
+			}
+
+			if(bipNode->getChildren().at(1)->getNodeType() == END_) {
+
+			}
+
+			if(bipNode->getChildren().at(1)->getNodeType() != END_ && bipNode->getChildren().at(1)->getNodeType() != DUMMY_) {
+				unordered_set<int> nextSet = stmt->getNextBip();
+				BOOST_FOREACH(auto next, nextSet) {
+					Statement* nextStmt = stmtTable->getStmtObj(next);
+					dfsFind(nextStmt, str, visited, entrance);
+				}
+			}
+		} else {
+
+		}
+	}
+}
+
+bool NextStarBipClause::contains(vector<string> arr, string item) {
+	if(find(arr.begin(), arr.end(), item) != arr.end()) {
+		return true;
+	}
+
+	return false;
+}
+
+bool NextStarBipClause::hasEndNode(vector<GNode*> children) {
+	BOOST_FOREACH(auto child, children) {
+		if(child->getNodeType() == END_) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int NextStarBipClause::getNodePosition(vector<GNode*> arr, GNode* item) {
+	int pos = find(arr.begin(), arr.end(), item) - arr.begin();
+
+	return pos;
+}
+
+unordered_set<Statement*> NextStarBipClause::getNeededStmts(string type) {
+	unordered_set<Statement*> stmts;
+	
+	if(type == ARG_STATEMENT || type == ARG_PROGLINE) {
+		return stmtTable->getAllStmts();
+	}
+
+	if(type == ARG_IF) {
+		return stmtTable->getIfStmts();
+	}
+
+	if(type == ARG_WHILE) {
+		return stmtTable->getWhileStmts();
+	}
+
+	if(type == ARG_CALL) {
+		return stmtTable->getCallStmts();
+	}
+
+	if(type == ARG_ASSIGN) {
+		return stmtTable->getAssgStmts();
+	}
+
+	return stmts;
+}
+
+bool NextStarBipClause::isNeededArgType(string type, int stmtNum) {
+	Statement* stmt = stmtTable->getStmtObj(stmtNum);
+
+	if(type == ARG_STATEMENT || type == ARG_PROGLINE) {
+		return true;
+	}
+	
+	if(stmt->getType() == ASSIGN_STMT_ && type == ARG_ASSIGN) {
+		return true;
+	}
+
+	if(stmt->getType() == WHILE_STMT_ && type == ARG_WHILE) {
+		return true;
+	}
+
+	if(stmt->getType() == CALL_STMT_ && type == ARG_CALL) {
+		return true;
+	}
+
+	if(stmt->getType() == IF_STMT_ && type == ARG_IF) {
+		return true;
+	}
+
+	return false;
+}
---
+++ SPA/NextStarBipClause.h
@@ -0,0 +1,61 @@
+/*
+ * NextStarBipClause.h
+ *
+ *  Created on: 2 Nov, 2015
+ *      Author: Leon
+ */
+
+#pragma once
+
+#include "CFGbip.h"
+#include "SuchThatClause.h"
+#include "StmtTable.h"
+
+using namespace boost;
+
+class NextStarBipClause
+	: public SuchThatClause {
+
+public:
+	NextStarBipClause(void);
+	~NextStarBipClause(void);
+
+	bool isValid(void);
+
+protected:
+
+	//e.g. Parent(string,string)
+	bool evaluateS1FixedS2Fixed(string, string);
+	//e.g. Parent(_,_)
+	bool evaluateS1GenericS2Generic();
+	//e.g. Parent(_,string)
+	bool evaluateS1GenericS2Fixed(string);
+	//Parent(string,_)
+	bool evaluateS1FixedS2Generic(string);
+	//Parent(string,s2)
+	unordered_set<string> getAllS2WithS1Fixed(string);
+	//Parent(_,s2)
+	unordered_set<string> getAllS2();
+	//Parent(s1,string)
+	unordered_set<string> getAllS1WithS2Fixed(string);
+	//Parent(s1,__)
+	unordered_set<string> getAllS1();
+	//Parent(s1,s2)
+	unordered_set<vector<string>> getAllS1AndS2();
+
+private:
+	CFGbip* cfg;
+	bool match;
+	unordered_set<string> results;
+	unordered_set<vector<string>> resultsPair;
+
+	void dfsFind(Statement*, string, vector<string>, stack<int>);
+
+	bool contains(vector<string>, string);
+	bool hasEndNode(vector<GNode*>);
+
+	int getNodePosition(vector<GNode*>, GNode*);
+
+	unordered_set<Statement*> getNeededStmts(string);
+	bool isNeededArgType(string, int);
+};
