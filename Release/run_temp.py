ef41065b555fcb81e893e82599cdf78afa78e3ef
Jonathan Chiam <jczjchiam@gmail.com>
2015-11-07 16:01:59 +0800

    NextBIP test done

--- Release/run_temp.py
+++ Release/run_temp.py
@@ -1,6 +1,6 @@
 import os
 
-# SYSTEM SOURCE 1
+# SYSTEM SOURCES
 # os.system("AutoTester SystemTests/Sources/System-Source-1.txt SystemTests/SingleProc-ss1.txt SystemTests/Results/singleproc-ss1.xml")
 # os.system("AutoTester SystemTests/Sources/System-Source-2.txt SystemTests/MultiProc-ss2.txt SystemTests/Results/multiproc-ss2.xml")
 
@@ -35,3 +35,7 @@ import os
 # WITH CLAUSE
 # os.system("AutoTester SystemTests/Sources/System-Source-1.txt SystemTests/With-ss1.txt SystemTests/Results/with-ss1.xml")
 # os.system("AutoTester SystemTests/Sources/System-Source-2.txt SystemTests/With-ss2.txt SystemTests/Results/with-ss2.xml")
+
+# EXTENSION
+os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/NextBIP-ss3.txt SystemTests/Reuslts/nextbip-ss3.xml")
+# os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/NextBIPStar-ss3.txt SystemTests/Reuslts/nextbipstar-ss3.xml")
