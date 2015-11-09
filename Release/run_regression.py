054f23858db5c5af6e7acf8ed2d5c3e9206b1260
Jonathan Chiam <jczjchiam@gmail.com>
2015-11-09 08:25:21 +0800

    Test script edits

--- Release/run_regression.py
+++ Release/run_regression.py
@@ -2,11 +2,7 @@ import os
 
 os.system("AutoTester SystemTests/Sources/System-Source-1.txt SystemTests/SingleProc-ss1.txt SystemTests/Results/singleproc-ss1.xml")
 os.system("AutoTester SystemTests/Sources/System-Source-2.txt SystemTests/MultiProc-ss2.txt SystemTests/Results/multiproc-ss2.xml")
-
-os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/Next-ss3.txt SystemTests/Results/next-ss3.xml")
-os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/NextStar-ss3.txt SystemTests/Results/nextstar-ss3.xml")
-os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/Affects-ss3.txt SystemTests/Results/affects-ss3.xml")
-os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/AffectsStar-ss3.txt SystemTests/Results/affectsstar-ss3.xml")
+os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/MultiProc-ss3.txt SystemTests/Results/multiproc-ss3.xml")
 
 # EXTENSION
 # os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/NextBIP-ss3.txt SystemTests/Reuslts/nextbip-ss3.xml")
--- Release/run_temp.py
+++ Release/run_temp.py
@@ -3,6 +3,7 @@ import os
 # SYSTEM SOURCES
 # os.system("AutoTester SystemTests/Sources/System-Source-1.txt SystemTests/SingleProc-ss1.txt SystemTests/Results/singleproc-ss1.xml")
 # os.system("AutoTester SystemTests/Sources/System-Source-2.txt SystemTests/MultiProc-ss2.txt SystemTests/Results/multiproc-ss2.xml")
+# os.system("AutoTester SystemTests/Sources/System-Source-3.txt SystemTests/MultiProc-ss3.txt SystemTests/Results/multiproc-ss3.xml")
 
 # SUCH THAT CLAUSES
 # os.system("AutoTester SystemTests/Sources/System-Source-1.txt SystemTests/Uses-ss1.txt SystemTests/Results/uses-ss1.xml")
